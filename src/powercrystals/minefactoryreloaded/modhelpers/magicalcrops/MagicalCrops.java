3e3f75da3ebe115ad86af2d7cea36857978d5134
skyboy <skyboy026@gmail.com>
2013-10-24 04:13:55 -0400

    Fix magical crops integration

--- src/powercrystals/minefactoryreloaded/modhelpers/magicalcrops/MagicalCrops.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/magicalcrops/MagicalCrops.java
@@ -11,7 +11,6 @@ import powercrystals.minefactoryreloaded.api.FertilizerType;
 import powercrystals.minefactoryreloaded.farmables.fertilizables.FertilizerStandard;
 import powercrystals.minefactoryreloaded.farmables.harvestables.HarvestableCropPlant;
 import powercrystals.minefactoryreloaded.farmables.plantables.PlantableCropPlant;
-import powercrystals.minefactoryreloaded.modhelpers.FertilizableCropReflection;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
@@ -19,7 +18,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
-@Mod(modid = "MineFactoryReloaded|CompatMagicalCrops", name = "MFR Compat: Magical Crops", version = MineFactoryReloadedCore.version, dependencies = "after:MineFactoryReloaded;after:Magical-Crops")
+@Mod(modid = "MineFactoryReloaded|CompatMagicalCrops", name = "MFR Compat: Magical Crops", version = MineFactoryReloadedCore.version, dependencies = "after:MineFactoryReloaded;after:magicalcrops")
 @NetworkMod(clientSideRequired = false, serverSideRequired = false)
 public class MagicalCrops
 {
@@ -28,50 +27,46 @@ public class MagicalCrops
 	@EventHandler
 	public static void load(FMLInitializationEvent e)
 	{
-		if(!Loader.isModLoaded("Magical-Crops"))
+		if(!Loader.isModLoaded("magicalcrops"))
 		{
 			FMLLog.warning("Magical Crops missing - MFR Compat: Magical Crops not loading");
 			return;
 		}
 		try
 		{
-			Class<?> mod = Class.forName("magicCrop.mod_mCrops");
+			Class<?> mod = Class.forName("magicalcrops.mod_mCrops");
+			
+			Item magicalFertilizer = (Item)mod.getField("MagicFertilizer").get(null);
+			if(magicalFertilizer != null)
+			{
+				MFRRegistry.registerFertilizer(new FertilizerStandard(magicalFertilizer.itemID, 0, FertilizerType.GrowMagicalCrop));
+			}
 			
 			// the various plants are separated by type to make future changes easier (mostly considering magicFertilizer behavior)
-			String[] crops = {"Sberry", "Tomato", "Sweetcorn", "Cucum", "Melon", "Bberry", "Rberry", "Grape", "Chil"};
-			String[] namedAsMagicalCropButExtendsBlockCrops = {"Nether"};
-			String[] magicalCrops = {"Coal", "Dye", "Iron", "Redstone", "Glowstone", "Gold", "Diamond", "Lapis", "Blaze", "Emerald", "Ender", "Gunpowder", "XP", "Copper", "Tin", "Obsidian"};
-			String[] soulCrops = {"Cow", "Pigmen", "Skele", "Spider"};
+			//String[] crops = {"Sberry", "Tomato", "Sweetcorn", "Cucum", "Melon", "Bberry", "Rberry", "Grape", "Chil"};
+			String[] magicalCrops = { "Coal", "Redstone", "Glowstone", "Obsidian", "Dye", "Iron", "Gold",
+									"Lapis", "Ender", "Nether", "XP", "Blaze", "Diamond", "Emerald" };
+			String[] soulCrops = { "Cow", "Creeper", "Magma", "Skeleton", "Slime", "Spider", "Ghast" };
 			
 			int seedId;
 			int blockId;
 			Method fertilize;
 			
-			for(String crop : crops)
+			/*for(String crop : crops)
 			{
 				seedId = ((Item)mod.getField("seed" + crop).get(null)).itemID;
 				blockId = ((Block)mod.getField("crop" + crop).get(null)).blockID;
-				fertilize = Class.forName("magicCrop.crop" + crop).getMethod("func_72272_c_", World.class, int.class, int.class, int.class);
+				fertilize = Class.forName("magicalcrops.crop" + crop).getMethod("func_72272_c_", World.class, int.class, int.class, int.class);
 				MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
 				MFRRegistry.registerHarvestable(new HarvestableCropPlant(blockId, 7));
 				MFRRegistry.registerFertilizable(new FertilizableCropReflection(blockId, fertilize, 7));
-			}
-			
-			for(String crop : namedAsMagicalCropButExtendsBlockCrops)
-			{
-				seedId = ((Item)mod.getField("mSeeds" + crop).get(null)).itemID;
-				blockId = ((Block)mod.getField("mCrop" + crop).get(null)).blockID;
-				fertilize = Class.forName("magicCrop.mCrop" + crop).getMethod("func_72272_c_", World.class, int.class, int.class, int.class);
-				MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
-				MFRRegistry.registerHarvestable(new HarvestableCropPlant(blockId, 7));
-				MFRRegistry.registerFertilizable(new FertilizableCropReflection(blockId, fertilize, 7));
-			}
+			}//*/
 			
 			for(String magicalCrop : magicalCrops)
 			{
 				seedId = ((Item)mod.getField("mSeeds" + magicalCrop).get(null)).itemID;
 				blockId = ((Block)mod.getField("mCrop" + magicalCrop).get(null)).blockID;
-				fertilize = Class.forName("magicCrop.mCrop" + magicalCrop).getMethod("fertilize", World.class, int.class, int.class, int.class);
+				fertilize = Class.forName("magicalcrops.mCrop" + magicalCrop).getMethod("fertilize", World.class, int.class, int.class, int.class);
 				MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
 				MFRRegistry.registerHarvestable(new HarvestableCropPlant(blockId, 7));
 				MFRRegistry.registerFertilizable(new FertilizableMagicalCropReflection(blockId, fertilize, 7));
@@ -79,18 +74,12 @@ public class MagicalCrops
 			
 			for(String soulCrop : soulCrops)
 			{
-				seedId = ((Item)mod.getField("soulSeed" + soulCrop).get(null)).itemID;
+				seedId = ((Item)mod.getField("sSeeds" + soulCrop).get(null)).itemID;
 				blockId = ((Block)mod.getField("soulCrop" + soulCrop).get(null)).blockID;
-				fertilize = Class.forName("magicCrop.soulCrop" + soulCrop).getMethod("fertilize", World.class, int.class, int.class, int.class);
+				fertilize = Class.forName("magicalcrops.soulCrop" + soulCrop).getMethod("fertilize", World.class, int.class, int.class, int.class);
 				MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
 				MFRRegistry.registerHarvestable(new HarvestableCropPlant(blockId, 7));
 				MFRRegistry.registerFertilizable(new FertilizableMagicalCropReflection(blockId, fertilize, 7));
-			}
-			
-			Item magicalFertilizer = (Item)mod.getField("magicFertilizer").get(null);
-			if(magicalFertilizer != null)
-			{
-				MFRRegistry.registerFertilizer(new FertilizerStandard(magicalFertilizer.itemID, 0, FertilizerType.GrowMagicalCrop));
 			}
 			
 		}
