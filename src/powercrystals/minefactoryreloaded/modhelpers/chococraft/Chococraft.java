87623bb2ece4f8096c6a04d980bd8e35172a9173
skyboy <skyboy026@gmail.com>
2013-10-28 03:05:47 -0400

    Remove grinder helper for chococraft

--- src/powercrystals/minefactoryreloaded/modhelpers/chococraft/Chococraft.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/chococraft/Chococraft.java
@@ -38,25 +38,6 @@ public class Chococraft
 			MFRRegistry.registerPlantable(new PlantableCropPlant(seedId, blockId));
 			MFRRegistry.registerHarvestable(new HarvestableChococraft(blockId));
 			MFRRegistry.registerFertilizable(new FertilizableChococraft(blockId));
-			
-			// Chocobo List
-			FMLLog.info("Registering Chocobos for Grinder...");
-			final String[] chocobos = { "Black", "Blue", "Gold", "Green", "Pink", "Purple", "Red", "White", "Yellow" };
-			int legItem = ((Item)(mod.getField("chocoboLegRawItem").get(null))).itemID;
-			int featherItem = ((Item)(mod.getField("chocoboFeatherItem").get(null))).itemID;
-			
-			for (String name : chocobos)
-			{
-				try
-				{
-					Class<?> chocoboClass = Class.forName(String.format("chococraft.common.entities.colours.EntityChocobo%s", name));
-					MFRRegistry.registerGrindable(new GrindableChocobo(chocoboClass, legItem, featherItem));
-				}
-				catch (ClassNotFoundException e)
-				{
-					FMLLog.warning(String.format("Entity class for the %s Chocobo could not be found", name));
-				}
-			}
 		}
 		catch (ClassNotFoundException e)
 		{
