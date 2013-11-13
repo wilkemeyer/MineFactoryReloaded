d0ce7371808a4e067a222d2f5778f677f9954257
skyboy <skyboy026@gmail.com>
2013-11-13 04:14:09 -0500

    Squids can't be ranched rapidly

--- src/powercrystals/minefactoryreloaded/farmables/ranchables/RanchableSquid.java
+++ src/powercrystals/minefactoryreloaded/farmables/ranchables/RanchableSquid.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntitySquid;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import powercrystals.minefactoryreloaded.api.IFactoryRanchable;
 import powercrystals.minefactoryreloaded.api.RanchedItem;
@@ -22,6 +23,11 @@ public class RanchableSquid implements IFactoryRanchable
 	@Override
 	public List<RanchedItem> ranch(World world, EntityLivingBase entity, IInventory rancher)
 	{
+		NBTTagCompound tag = entity.getEntityData();
+		if (tag.getLong("mfr:lastRanched") > world.getWorldTime())
+			return null;
+		tag.setLong("mfr:lastRanched", world.getWorldTime() + 20 * 5);
+		
 		List<RanchedItem> drops = new ArrayList<RanchedItem>();
 		drops.add(new RanchedItem(Item.dyePowder, 1, 0));
 		return drops;
