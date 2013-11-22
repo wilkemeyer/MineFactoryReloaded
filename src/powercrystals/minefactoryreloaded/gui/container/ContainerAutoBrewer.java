8a2c76409db1a34a60c5ebbe09ff2ab1b04b9f89
skyboy <skyboy026@gmail.com>
2013-11-22 18:13:18 -0500

    Intelligent shift-click on the auto-brewer

--- src/powercrystals/minefactoryreloaded/gui/container/ContainerAutoBrewer.java
+++ src/powercrystals/minefactoryreloaded/gui/container/ContainerAutoBrewer.java
@@ -2,6 +2,7 @@ package powercrystals.minefactoryreloaded.gui.container;
 
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import powercrystals.minefactoryreloaded.gui.slot.SlotFake;
 import powercrystals.minefactoryreloaded.gui.slot.SlotPotionIngredient;
 import powercrystals.minefactoryreloaded.tile.base.TileEntityFactoryPowered;
@@ -31,5 +32,102 @@ public class ContainerAutoBrewer extends ContainerFactoryPowered
 	protected int getPlayerInventoryVerticalOffset()
 	{
 		return 174;
+	}
+	
+	@Override
+	protected boolean mergeItemStack(ItemStack stack, int slotStart, int slotRange, boolean reverse)
+	{ // TODO: merge this into ContainerFactoryInventory?
+		boolean successful = false;
+		int slotIndex = slotStart;
+		int maxStack = Math.min(stack.getMaxStackSize(), _te.getInventoryStackLimit());
+		int machineEnd = _te.getSizeInventory();
+		
+		if(reverse)
+		{
+			slotIndex = slotRange - 1;
+		}
+		
+		Slot slot;
+		ItemStack existingStack;
+		
+		if(stack.isStackable())
+		{
+			while(stack.stackSize > 0 && (!reverse && slotIndex < slotRange || reverse && slotIndex >= slotStart))
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				existingStack = slot.getStack();
+				
+				l: if(slot.isItemValid(stack) && existingStack != null && existingStack.itemID == stack.itemID && (!stack.getHasSubtypes() || stack.getItemDamage() == existingStack.getItemDamage()) && ItemStack.areItemStackTagsEqual(stack, existingStack))
+				{
+					if (slotIndex < machineEnd && !_te.canInsertItem(slotIndex, stack, -1))
+						break l;
+					int existingSize = existingStack.stackSize + stack.stackSize;
+					
+					if(existingSize <= maxStack)
+					{
+						stack.stackSize = 0;
+						existingStack.stackSize = existingSize;
+						slot.onSlotChanged();
+						successful = true;
+					}
+					else if (existingStack.stackSize < maxStack)
+					{
+						stack.stackSize -= maxStack - existingStack.stackSize;
+						existingStack.stackSize = maxStack;
+						slot.onSlotChanged();
+						successful = true;
+					}
+				}
+				
+				if(reverse)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+		
+		if(stack.stackSize > 0)
+		{
+			if(reverse)
+			{
+				slotIndex = slotRange - 1;
+			}
+			else
+			{
+				slotIndex = slotStart;
+			}
+			
+			while(!reverse && slotIndex < slotRange || reverse && slotIndex >= slotStart)
+			{
+				slot = (Slot)this.inventorySlots.get(slotIndex);
+				existingStack = slot.getStack();
+				
+				l: if(slot.isItemValid(stack) && existingStack == null)
+				{
+					if (slotIndex < machineEnd && !_te.canInsertItem(slotIndex, stack, -1))
+						break l;
+					slot.putStack(stack.copy());
+					slot.onSlotChanged();
+					stack.stackSize = 0;
+					successful = true;
+					break;
+				}
+				
+				if(reverse)
+				{
+					--slotIndex;
+				}
+				else
+				{
+					++slotIndex;
+				}
+			}
+		}
+		
+		return successful;
 	}
 }
