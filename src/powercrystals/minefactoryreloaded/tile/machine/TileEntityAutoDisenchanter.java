b07bb2c8ad06d41fd476ae9890e24e70a4188bcd
skyboy <skyboy026@gmail.com>
2014-01-14 22:29:55 -0500

    Ensure minimum value to rand is 1

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoDisenchanter.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoDisenchanter.java
@@ -128,7 +128,7 @@ public class TileEntityAutoDisenchanter extends TileEntityFactoryPowered
 					
 					if(_inventory[0].isItemStackDamageable())
 					{
-						int damage = worldObj.rand.nextInt((int)(_inventory[0].getMaxDamage() * 0.25)) + (int)(_inventory[0].getMaxDamage() * 0.1);
+						int damage = worldObj.rand.nextInt(1 + (int)(_inventory[0].getMaxDamage() * 0.25)) + (int)(_inventory[0].getMaxDamage() * 0.1);
 						_inventory[0].setItemDamage(_inventory[0].getItemDamage() + damage);
 						if(_inventory[0].getItemDamage() >= _inventory[0].getMaxDamage())
 						{
