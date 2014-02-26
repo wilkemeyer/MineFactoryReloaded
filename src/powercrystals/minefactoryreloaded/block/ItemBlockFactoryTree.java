c9b0b21f1166ce1d7488dc667204f80f4a98a723
skyboy <skyboy026@gmail.com>
2014-02-26 01:37:06 -0500

    Add warning to sacred rubber sapling

--- src/powercrystals/minefactoryreloaded/block/ItemBlockFactoryTree.java
+++ src/powercrystals/minefactoryreloaded/block/ItemBlockFactoryTree.java
@@ -1,5 +1,13 @@
 package powercrystals.minefactoryreloaded.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
 public class ItemBlockFactoryTree extends ItemBlockFactory
 {
 	public ItemBlockFactoryTree(int id)
@@ -7,5 +15,14 @@ public class ItemBlockFactoryTree extends ItemBlockFactory
 		super(id);
 		setNames(new String[] {null, "massive"});
 		setHasSubtypes(true);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List info, boolean adv)
+	{
+		if (stack.getItemDamage() == 1)
+			info.add("Warning: Pakratt's world ship.");
 	}
 }
