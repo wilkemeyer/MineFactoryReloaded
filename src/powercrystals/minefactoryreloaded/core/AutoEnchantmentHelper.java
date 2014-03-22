da1ecfdc1543dfa428005c19dfbdb7b591e14ff6
skyboy <skyboy026@gmail.com>
2014-03-22 09:14:39 -0400

    Fix crash with enchantment helper

--- src/powercrystals/minefactoryreloaded/core/AutoEnchantmentHelper.java
+++ src/powercrystals/minefactoryreloaded/core/AutoEnchantmentHelper.java
@@ -1,12 +1,15 @@
 package powercrystals.minefactoryreloaded.core;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentData;
@@ -23,7 +26,7 @@ public class AutoEnchantmentHelper extends EnchantmentHelper
 	{
 		return addRandomEnchantment(rand, stack, level, false);
 	}
-	
+
 	public static ItemStack addRandomEnchantment(Random rand, ItemStack stack, int level, boolean invalid)
 	{
 		if (stack == null)
@@ -31,25 +34,25 @@ public class AutoEnchantmentHelper extends EnchantmentHelper
 			return null;
 		}
 		ItemStack output = stack.splitStack(1);
-		
+
 		List<EnchantmentData> enchantments = buildEnchantmentList(rand, output, level, !invalid);
 		if(enchantments == null)
 		{
 			return output;
 		}
-		
+
 		Map<Integer, Integer> existingEnchants = getEnchantments(output);
-		
+
 		boolean isBook = output.itemID == Item.book.itemID;
-		
-		
+
+
 		if(isBook)
 		{
 			output.itemID = Item.enchantedBook.itemID;
 		}
-		
+
 		Collections.shuffle(enchantments);
-		
+
 		outerlist:	for(EnchantmentData newEnchant : enchantments)
 		{
 			if(isBook)
@@ -73,10 +76,10 @@ public class AutoEnchantmentHelper extends EnchantmentHelper
 				output.addEnchantment(newEnchant.enchantmentobj, newEnchant.enchantmentLevel);
 			}
 		}
-		
+
 		return output;
 	}
-	
+
 	private static void updateEnchantment(ItemStack stack, int enchantId, short newLevel)
 	{
 		NBTTagList tagList = stack.getTagCompound().getTagList("ench");
@@ -89,11 +92,12 @@ public class AutoEnchantmentHelper extends EnchantmentHelper
 		}
 		stack.getTagCompound().setTag("ench", tagList);
 	}
-	
-	public static List<EnchantmentData> buildEnchantmentList(Random rand, ItemStack stack, int level, boolean blockInvalid)
+
+	public static List<EnchantmentData> buildEnchantmentList(Random rand, ItemStack stack,
+			int level, boolean blockInvalid)
 	{
 		int itemEnchantability = stack.getItem().getItemEnchantability();
-		
+
 		if(itemEnchantability <= 0)
 		{
 			return null;
@@ -101,87 +105,88 @@ public class AutoEnchantmentHelper extends EnchantmentHelper
 		else
 		{
 			itemEnchantability /= 2;
-			itemEnchantability = 1 + rand.nextInt((itemEnchantability >> 1) + 1) + rand.nextInt((itemEnchantability >> 1) + 1);
+			itemEnchantability = 1 + rand.nextInt((itemEnchantability >> 1) + 1) +
+					rand.nextInt((itemEnchantability >> 1) + 1);
 			int var5 = itemEnchantability + level;
 			float var6 = (rand.nextFloat() + rand.nextFloat() - 1.0F) * 0.15F;
 			int targetEnchantability = (int)(var5 * (1.0F + var6) + 0.5F);
-			
+
 			if(targetEnchantability < 1)
 			{
 				targetEnchantability = 1;
 			}
-			
+
 			List<EnchantmentData> enchantmentList = null;
 			Map<Integer, EnchantmentData> enchantmentMap = mapEnchantmentData(targetEnchantability, stack);
-			
+
 			if(enchantmentMap != null && !enchantmentMap.isEmpty())
 			{
-				EnchantmentData enchData = (EnchantmentData)WeightedRandom.getRandomItem(rand, enchantmentMap.values());
-				
+				Collection<EnchantmentData> map = enchantmentMap.values();
+				EnchantmentData enchData = (EnchantmentData)WeightedRandom.getRandomItem(rand, map);
+
 				if(enchData != null)
 				{
 					enchantmentList = new ArrayList<EnchantmentData>();
 					enchantmentList.add(enchData);
-					
+					Set<Integer> mapSet = enchantmentMap.keySet();
+
 					for(int i = targetEnchantability; rand.nextInt(50) <= i; i >>= 1)
 					{
-						if (blockInvalid) for (Integer enchIndex : enchantmentMap.keySet())
+						if (blockInvalid) for (Iterator<Integer> iter = mapSet.iterator(); iter.hasNext();)
 						{
-							boolean validWithPreviousEnchants = true;
+							Enchantment ench = Enchantment.enchantmentsList[iter.next()];
 							for(EnchantmentData newEnchantment : enchantmentList)
-							{
-								if (newEnchantment.enchantmentobj.canApplyTogether(Enchantment.enchantmentsList[enchIndex]))
+								if (!newEnchantment.enchantmentobj.canApplyTogether(ench))
 								{
-									continue;
-								}
-								
-								validWithPreviousEnchants = false;
-							}
-							if(!validWithPreviousEnchants)
-							{
-								enchantmentMap.remove(enchIndex);
-							}
+									iter.remove();
+									break;
+								} // TODO: this inner loop probably isn't needed
 						}
-						
+
 						if(!enchantmentMap.isEmpty())
 						{
-							EnchantmentData randomEnchant = (EnchantmentData)WeightedRandom.getRandomItem(rand, enchantmentMap.values());
+							EnchantmentData randomEnchant = (EnchantmentData)WeightedRandom.
+									getRandomItem(rand, map);
 							enchantmentList.add(randomEnchant);
 						}
+						else
+							break;
 					}
 				}
 			}
-			
+
 			return enchantmentList;
 		}
 	}
-	
+
 	public static Map<Integer, EnchantmentData> mapEnchantmentData(int targetEnchantability, ItemStack stack)
 	{
 		HashMap<Integer, EnchantmentData> enchantmentMap = null;
 		boolean isBook = stack.itemID == Item.book.itemID;
-		
+
 		for(int var7 = 0; var7 < Enchantment.enchantmentsList.length; ++var7)
 		{
 			Enchantment enchantment = Enchantment.enchantmentsList[var7];
-			
+
 			if(enchantment != null && (isBook || enchantment.canApplyAtEnchantingTable(stack)))
 			{
 				for(int enchLevel = enchantment.getMinLevel(); enchLevel <= enchantment.getMaxLevel(); ++enchLevel)
 				{
-					if(targetEnchantability >= enchantment.getMinEnchantability(enchLevel) && targetEnchantability <= enchantment.getMaxEnchantability(enchLevel))
+					if(targetEnchantability >= enchantment.getMinEnchantability(enchLevel) &&
+							targetEnchantability <= enchantment.getMaxEnchantability(enchLevel))
 					{
 						if(enchantmentMap == null)
 						{
 							enchantmentMap = new HashMap<Integer, EnchantmentData>();
 						}
-						
-						enchantmentMap.put(Integer.valueOf(enchantment.effectId), new EnchantmentData(enchantment, enchLevel));
+
+						enchantmentMap.put(Integer.valueOf(enchantment.effectId),
+								new EnchantmentData(enchantment, enchLevel));
 					}
 				}
 			}
 		}
-		
+
 		return enchantmentMap;
 	}
 }
