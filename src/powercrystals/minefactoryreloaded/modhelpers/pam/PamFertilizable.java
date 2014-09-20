a7f64f092f4357d5c42f0dd738f026acdc6e881b
skyboy <skyboy026@gmail.com>
2014-09-20 14:22:25 -0400

    HarvestCraft integration now part of HarvestCraft

--- src/powercrystals/minefactoryreloaded/modhelpers/pam/PamFertilizable.java
+++
@@ -1,65 +0,0 @@
-package powercrystals.minefactoryreloaded.modhelpers.pam;import java.lang.reflect.Method;import java.util.Random;import net.minecraft.block.Block;import net.minecraft.tileentity.TileEntity;import net.minecraft.world.World;import powercrystals.minefactoryreloaded.api.FertilizerType;import powercrystals.minefactoryreloaded.api.IFactoryFertilizable;
-class PamFertilizable implements IFactoryFertilizable
-{
-	protected Method getGrowthStage;
-	protected Method _fertilize;
-	private Block _blockId;
-	final private Object[] dummyArgs = new Object[]{};
-	public PamFertilizable(Block blockId) throws ClassNotFoundException
-	{
-		_blockId = blockId;
-		getGrowthStage=Pam.pamTEGetGrowthStage;
-		_fertilize=Pam.pamBlockFertilize;
-	}
-	@Override
-	public Block getPlant()
-	{
-		return _blockId;
-	}
-
-	@Override
-	public boolean canFertilize(World world, int x, int y, int z, FertilizerType fertilizerType)
-	{
-		int stage=0;
-		TileEntity te=world.getTileEntity(x,y,z);
-		if(getGrowthStage!=null&&te!=null)
-		{
-			try
-			{
-				stage=(Integer)(getGrowthStage.invoke(te,dummyArgs));
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		return stage < 2 && fertilizerType == FertilizerType.GrowPlant;
-	}
-	@Override
-	public boolean fertilize(World world, Random rand, int x, int y, int z, FertilizerType fertilizerType)
-	{
-		int stage=0;
-		TileEntity te=world.getTileEntity(x,y,z);
-		if(getGrowthStage!=null&&te!=null)
-		{
-			try
-			{
-				stage=(Integer)(getGrowthStage.invoke(te,dummyArgs));
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		if (stage>=2 )
-			return false;
-		try
-		{
-			_fertilize.invoke(_blockId, world, x, y, z);
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		return true;
-	}}
--- src/powercrystals/minefactoryreloaded/modhelpers/pam/PlantablePamCrop.java
+++
@@ -1,80 +0,0 @@
-package powercrystals.minefactoryreloaded.modhelpers.pam;
-import cpw.mods.fml.common.FMLLog;import java.lang.reflect.InvocationTargetException;import java.lang.reflect.Method;import net.minecraft.block.Block;import net.minecraft.init.Blocks;import net.minecraft.item.Item;import net.minecraft.item.ItemStack;import net.minecraft.tileentity.TileEntity;import net.minecraft.world.World;import net.minecraftforge.common.util.ForgeDirection;import net.minecraftforge.common.IPlantable;import powercrystals.minefactoryreloaded.api.IFactoryPlantable;import powercrystals.minefactoryreloaded.api.ReplacementBlock;
-public class PlantablePamCrop implements IFactoryPlantable
-{
-	protected Block _blockId;
-	protected Item _itemId;
-	protected ReplacementBlock _plantableBlockId;
-	protected int _cropId;
-	protected Method _setCrop;
-	protected Method _setStage;
-	public PlantablePamCrop(Block blockId, Item itemId,int cropId)
-	{
-		_blockId = blockId;
-		_itemId = itemId;
-		_plantableBlockId = new ReplacementBlock(blockId);
-		_cropId=cropId;
-		_setCrop=Pam.pamTESetCropId;
-		_setStage=Pam.pamTESetGrowthStage;
-	}
-	@Override
-	public Item getSeed()
-	{
-		return _itemId;
-	}	@Override	public boolean canBePlanted(ItemStack stack, boolean forFermenting)	{		// Auto-generated method stub		return true;	}
-	@Override
-	public ReplacementBlock getPlantedBlock(World world, int x, int y, int z, ItemStack stack)
-	{
-		return _plantableBlockId;
-	}
-	@Override
-	public boolean canBePlantedHere(World world, int x, int y, int z, ItemStack stack)
-	{
-		Block groundId = world.getBlock(x, y - 1, z);
-		if(!world.isAirBlock(x, y, z))
-		{
-			return false;
-		}
-		return (
-				groundId == Blocks.dirt ||
-				groundId == Blocks.grass ||
-				groundId == Blocks.farmland ||
-				_blockId instanceof IPlantable && 
-				groundId.canSustainPlant(world, x, y, z, ForgeDirection.UP, (IPlantable)_blockId));
-	}
-	@Override
-	public void prePlant(World world, int x, int y, int z, ItemStack stack)
-	{
-		Block groundId = world.getBlock(x, y - 1, z);
-		if(groundId == Blocks.dirt || groundId == Blocks.grass)
-		{
-			world.setBlock(x, y - 1, z, Blocks.farmland);
-		}
-	}
-
-	@Override
-	public void postPlant(World world, int x, int y, int z, ItemStack stack)
-	{
-		TileEntity te=world.getTileEntity(x,y,z);
-		try
-		{
-			if(te!=null)
-			{
-				_setCrop.invoke(te,_cropId);
-				_setStage.invoke(te,0);
-			}
-		}
-		catch (InvocationTargetException ex)
-		{
-			FMLLog.warning("Error with planting a crop from Pams mods");
-		}
-		catch (IllegalArgumentException ex)
-		{
-			FMLLog.warning("Error with planting a crop from Pams mods, this should not happen");
-		}
-		catch (IllegalAccessException ex)
-		{
-			FMLLog.warning("Error with planting a crop from Pams mods,this should not happen");
-		}
-	}
-}
