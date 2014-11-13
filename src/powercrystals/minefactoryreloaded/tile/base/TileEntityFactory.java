wilkemeyer/MineFactoryReloaded

commit 52eb077229ef960270d1aa1662ce7b86626443f6

Temporary fix for #297

src/powercrystals/minefactoryreloaded/tile/base/TileEntityFactory.java
@@ -75,10 +75,11 @@ public abstract class TileEntityFactory extends TileEntityBase
 		_forwardDirection = ForgeDirection.NORTH;
 	}
 
-	//@Override
-	public void cofh_validate()
+	@Override
+	public void validate()
 	{
 		onRotate();
+		super.validate();
 		if (worldObj.isRemote && hasHAM())
 		{
 			MineFactoryReloadedClient.addTileToAreaList(this);
@@ -212,7 +213,7 @@ public abstract class TileEntityFactory extends TileEntityBase
 
 	protected void onRotate()
 	{
-		if (worldObj.blockExists(xCoord, yCoord, zCoord))
+		if (!isInvalid() && worldObj.blockExists(xCoord, yCoord, zCoord))
 		{
 			MFRUtil.notifyNearbyBlocks(worldObj, xCoord, yCoord, zCoord, getBlockType());
 		}

commit 16de6b20fc2052f88dbbd9dbe25b0b80c25dfa03

Remove crash caused by 52eb077229ef960270d1aa1662ce7b86626443f6

src/powercrystals/minefactoryreloaded/tile/base/TileEntityFactory.java
@@ -78,6 +78,7 @@ public abstract class TileEntityFactory extends TileEntityBase
 	@Override
 	public void validate()
 	{
+		tileEntityInvalid = true;
 		onRotate();
 		super.validate();
 		if (worldObj.isRemote && hasHAM())