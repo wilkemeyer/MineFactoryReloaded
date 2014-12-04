d2eba3ade7a3a75e02e2f7757651f3bbb52174d1
skyboy <skyboy026@gmail.com>
2014-12-04 06:34:35 -0500

    Close #171 Auto-Anvil no longer consumes materials and do no work
    Slight (1.5 sec) delay added when materials will be consumed so the
    ghost-output may be inspected

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoAnvil.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoAnvil.java
@@ -102,7 +102,7 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 	@Override
 	protected boolean activateMachine()
 	{
-		if(_output == null || _inventory[2] != null)
+		if (_output == null || _inventory[2] != null)
 		{
 			return false;
 		}
@@ -116,7 +116,7 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 				{
 					if (!incrementWorkDone()) return false;
 
-					if(getWorkDone() >= getWorkMax())
+					if (getWorkDone() >= getWorkMax())
 					{
 						_inventory[0] = null;
 						_inventory[1] = null;
@@ -131,11 +131,11 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 				return false;
 			}
 
-			if(drain(_tanks[0], 4, false) != 4)
+			if (drain(_tanks[0], 4, false) != 4)
 			{
 				return false;
 			}
-			if(stackSizeToBeUsedInRepair > 0 && (_inventory[1] == null || _inventory[1].stackSize < stackSizeToBeUsedInRepair))
+			if (stackSizeToBeUsedInRepair > 0 && (_inventory[1] == null || _inventory[1].stackSize < stackSizeToBeUsedInRepair))
 			{
 				return false;
 			}
@@ -143,12 +143,12 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 			drain(_tanks[0], 4, true);
 			if (!incrementWorkDone()) return false;
 
-			if(getWorkDone() >= getWorkMax())
+			if (getWorkDone() >= getWorkMax())
 			{
 				_inventory[0] = null;
 				_inventory[2] = _output;
 
-				if(stackSizeToBeUsedInRepair > 0 && _inventory[1].stackSize > stackSizeToBeUsedInRepair)
+				if (stackSizeToBeUsedInRepair > 0 && _inventory[1].stackSize > stackSizeToBeUsedInRepair)
 				{
 					_inventory[1].stackSize -= stackSizeToBeUsedInRepair;
 				}
@@ -171,6 +171,9 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 		super.onFactoryInventoryChanged();
 
 		_output = getAnvilOutput();
+		if (_output != null) {
+			setIdleTicks(getIdleTicksMax());
+		}
 		setWorkDone(0);
 	}
 
@@ -233,138 +236,150 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 						return e.output;
 					}
 				}
-				enchantingWithBook = addedItem.getItem().equals(Items.enchanted_book) &&
-						Items.enchanted_book.func_92110_g(addedItem).tagCount() > 0;
+				{ // eclipse is indenting weird again
+					enchantingWithBook = addedItem.getItem().equals(Items.enchanted_book) &&
+							Items.enchanted_book.func_92110_g(addedItem).tagCount() > 0;
+				}
+				int addedEnchants = 0;
 
-						if (outputItem.isItemStackDamageable() &&
-								outputItem.getItem().getIsRepairable(outputItem, addedItem))
+				if (outputItem.isItemStackDamageable() &&
+						outputItem.getItem().getIsRepairable(outputItem, addedItem))
+				{
+					int currentDamage = Math.min(outputItem.getItemDamageForDisplay(), outputItem.getMaxDamage() / 4);
+
+					if (currentDamage <= 0)
+					{
+						return null;
+					}
+
+					int repairStackSize = 0;
+					for (; currentDamage > 0 && repairStackSize < addedItem.stackSize; repairStackSize++)
+					{
+						outputItem.setItemDamage(outputItem.getItemDamageForDisplay() - currentDamage);
+						totalEnchCost += Math.max(1, currentDamage / 100) + existingEnchantments.size();
+						currentDamage = Math.min(outputItem.getItemDamageForDisplay(), outputItem.getMaxDamage() / 4);
+					}
+
+					this.stackSizeToBeUsedInRepair = repairStackSize;
+					if (repairStackSize > 0)
+						++addedEnchants;
+				}
+				else
+				{
+					if (!enchantingWithBook && (!outputItem.getItem().equals(addedItem.getItem()) ||
+							!outputItem.isItemStackDamageable()))
+					{
+						return null;
+					}
+
+					if (outputItem.isItemStackDamageable() && !enchantingWithBook)
+					{
+						int currentDamage = outputItem.getMaxDamage() - outputItem.getItemDamageForDisplay();
+						int addedItemDamage = addedItem.getMaxDamage() - addedItem.getItemDamageForDisplay();
+						int newDamage = addedItemDamage + outputItem.getMaxDamage() * 12 / 100;
+						int leftoverDamage = currentDamage + newDamage;
+						int repairedDamage = outputItem.getMaxDamage() - leftoverDamage;
+
+						if (repairedDamage < 0)
 						{
-							int currentDamage = Math.min(outputItem.getItemDamageForDisplay(), outputItem.getMaxDamage() / 4);
+							repairedDamage = 0;
+						}
 
-							if(currentDamage <= 0)
-							{
-								return null;
-							}
+						if (repairedDamage < outputItem.getItemDamage())
+						{
+							++addedEnchants;
+							outputItem.setItemDamage(repairedDamage);
+							totalEnchCost += Math.max(1, newDamage / 100);
+						}
+					}
 
-							int repairStackSize = 0;
-							for (; currentDamage > 0 && repairStackSize < addedItem.stackSize; repairStackSize++)
-							{
-								outputItem.setItemDamage(outputItem.getItemDamageForDisplay() - currentDamage);
-								totalEnchCost += Math.max(1, currentDamage / 100) + existingEnchantments.size();
-								currentDamage = Math.min(outputItem.getItemDamageForDisplay(), outputItem.getMaxDamage() / 4);
-							}
+					@SuppressWarnings("unchecked")
+					Map<Integer, Integer> addedEnchantments = EnchantmentHelper.getEnchantments(addedItem);
 
-							this.stackSizeToBeUsedInRepair = repairStackSize;
+					for (Integer addedEnchId : addedEnchantments.keySet())
+					{
+						Enchantment enchantment = Enchantment.enchantmentsList[addedEnchId];
+						if (enchantment == null)
+							continue;
+						int existingEnchLevel = existingEnchantments.containsKey(addedEnchId) ? existingEnchantments.get(addedEnchId) : 0;
+						int addedEnchLevel = addedEnchantments.get(addedEnchId);
+						int newEnchLevel;
+
+						if (existingEnchLevel == addedEnchLevel)
+						{
+							++addedEnchLevel;
+							newEnchLevel = addedEnchLevel;
 						}
 						else
 						{
-							if (!enchantingWithBook && (!outputItem.getItem().equals(addedItem.getItem()) ||
-									!outputItem.isItemStackDamageable()))
+							newEnchLevel = Math.max(addedEnchLevel, existingEnchLevel);
+						}
+
+						addedEnchLevel = newEnchLevel;
+						int levelDifference = addedEnchLevel - existingEnchLevel;
+						boolean canEnchantmentBeAdded = enchantment.canApply(outputItem);
+
+						if (outputItem.getItem().equals(Items.enchanted_book))
+						{
+							canEnchantmentBeAdded = true;
+						}
+
+						for (Integer existingEnchId : existingEnchantments.keySet())
+						{
+							if (existingEnchId != addedEnchId && !enchantment.canApplyTogether(Enchantment.enchantmentsList[existingEnchId]))
 							{
-								return null;
-							}
-
-							if (outputItem.isItemStackDamageable() && !enchantingWithBook)
-							{
-								int currentDamage = outputItem.getMaxDamage() - outputItem.getItemDamageForDisplay();
-								int addedItemDamage = addedItem.getMaxDamage() - addedItem.getItemDamageForDisplay();
-								int newDamage = addedItemDamage + outputItem.getMaxDamage() * 12 / 100;
-								int leftoverDamage = currentDamage + newDamage;
-								int repairedDamage = outputItem.getMaxDamage() - leftoverDamage;
-
-								if (repairedDamage < 0)
-								{
-									repairedDamage = 0;
-								}
-
-								if (repairedDamage < outputItem.getItemDamage())
-								{
-									outputItem.setItemDamage(repairedDamage);
-									totalEnchCost += Math.max(1, newDamage / 100);
-								}
-							}
-
-							@SuppressWarnings("unchecked")
-							Map<Integer, Integer> addedEnchantments = EnchantmentHelper.getEnchantments(addedItem);
-
-							for (Integer addedEnchId : addedEnchantments.keySet())
-							{
-								Enchantment enchantment = Enchantment.enchantmentsList[addedEnchId];
-								if (enchantment == null)
-									continue;
-								int existingEnchLevel = existingEnchantments.containsKey(addedEnchId) ? existingEnchantments.get(addedEnchId) : 0;
-								int addedEnchLevel = addedEnchantments.get(addedEnchId);
-								int newEnchLevel;
-
-								if (existingEnchLevel == addedEnchLevel)
-								{
-									++addedEnchLevel;
-									newEnchLevel = addedEnchLevel;
-								}
-								else
-								{
-									newEnchLevel = Math.max(addedEnchLevel, existingEnchLevel);
-								}
-
-								addedEnchLevel = newEnchLevel;
-								int levelDifference = addedEnchLevel - existingEnchLevel;
-								boolean canEnchantmentBeAdded = enchantment.canApply(outputItem);
-
-								if (outputItem.getItem().equals(Items.enchanted_book))
-								{
-									canEnchantmentBeAdded = true;
-								}
-
-								for (Integer existingEnchId : existingEnchantments.keySet())
-								{
-									if (existingEnchId != addedEnchId && !enchantment.canApplyTogether(Enchantment.enchantmentsList[existingEnchId]))
-									{
-										canEnchantmentBeAdded = false;
-										totalEnchCost += levelDifference;
-									}
-								}
-
-								if (canEnchantmentBeAdded)
-								{
-									if (newEnchLevel > enchantment.getMaxLevel())
-									{
-										newEnchLevel = enchantment.getMaxLevel();
-									}
-
-									existingEnchantments.put(Integer.valueOf(addedEnchId), Integer.valueOf(newEnchLevel));
-									int enchCost = 0;
-
-									switch (enchantment.getWeight())
-									{
-									case 1:
-										enchCost = 8;
-										break;
-									case 2:
-										enchCost = 4;
-									case 3:
-									case 4:
-									case 6:
-									case 7:
-									case 8:
-									case 9:
-									default:
-										break;
-									case 5:
-										enchCost = 2;
-										break;
-									case 10:
-										enchCost = 1;
-									}
-
-									if (enchantingWithBook)
-									{
-										enchCost = Math.max(1, enchCost / 2);
-									}
-
-									totalEnchCost += enchCost * levelDifference;
-								}
+								canEnchantmentBeAdded = false;
+								totalEnchCost += levelDifference;
 							}
 						}
+
+						if (canEnchantmentBeAdded)
+						{
+							if (newEnchLevel > enchantment.getMaxLevel())
+							{
+								newEnchLevel = enchantment.getMaxLevel();
+							} else {
+								++addedEnchants;
+							}
+
+							existingEnchantments.put(Integer.valueOf(addedEnchId), Integer.valueOf(newEnchLevel));
+							int enchCost = 0;
+
+							switch (enchantment.getWeight())
+							{
+							case 1:
+								enchCost = 8;
+								break;
+							case 2:
+								enchCost = 4;
+							case 3:
+							case 4:
+							case 6:
+							case 7:
+							case 8:
+							case 9:
+							default:
+								break;
+							case 5:
+								enchCost = 2;
+								break;
+							case 10:
+								enchCost = 1;
+							}
+
+							if (enchantingWithBook)
+							{
+								enchCost = Math.max(1, enchCost / 2);
+							}
+
+							totalEnchCost += enchCost * levelDifference;
+						}
+					}
+				}
+
+				if (addedEnchants == 0) {
+					totalEnchCost = 0;
+				}
 			}
 
 			int enchCount = 0;
@@ -465,7 +480,7 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 	@Override
 	public int getIdleTicksMax()
 	{
-		return 1;
+		return 30;
 	}
 
 	@Override
