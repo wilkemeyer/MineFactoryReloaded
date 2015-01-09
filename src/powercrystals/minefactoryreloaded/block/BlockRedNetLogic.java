e99e9a80357b90011cd477af817e871aa65626a1
skyboy <skyboy026@gmail.com>
2015-01-09 02:52:51 -0500

    Fix #352 PRC TEs being mobile

--- src/powercrystals/minefactoryreloaded/block/BlockRedNetLogic.java
+++ src/powercrystals/minefactoryreloaded/block/BlockRedNetLogic.java
@@ -3,14 +3,12 @@ package powercrystals.minefactoryreloaded.block;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IChatComponent;
@@ -219,18 +217,6 @@ public class BlockRedNetLogic extends BlockFactory implements IRedNetOmniNode, I
 	public boolean isSideSolid(IBlockAccess world, int x, int y, int z, ForgeDirection side)
 	{
 		return side.ordinal() <= 1 || side.ordinal() >= 6 || world.getBlockMetadata(x, y, z) != _sideRemap[side.ordinal() - 2];
-	}
-
-	@Override
-	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
-	{
-		ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-		ItemStack prc = new ItemStack(getItemDropped(metadata, world.rand, fortune), 1, damageDropped(0));
-		NBTTagCompound tag = new NBTTagCompound();
-		world.getTileEntity(x, y, z).writeToNBT(tag);
-		prc.setTagCompound(tag);
-		drops.add(prc);
-		return drops;
 	}
 
 	@Override
