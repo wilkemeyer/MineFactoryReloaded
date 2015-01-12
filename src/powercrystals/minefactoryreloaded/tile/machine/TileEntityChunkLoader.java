wilkemeyer/MineFactoryReloaded

commit 8409f239593d58d6708eba76e67f6de353039fc8

(Probably broken) Support for IPortableData

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityChunkLoader.java
@@ -9,6 +9,7 @@ import gnu.trove.map.hash.TObjectIntHashMap;
 import java.lang.reflect.Field;
 import java.util.Set;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -44,14 +45,14 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 			f.setInt(tick, Short.MAX_VALUE);
 		} catch(Throwable _) {}
 	}
-	
+
 	protected static TObjectIntHashMap<String> fluidConsumptionRate = new TObjectIntHashMap<String>();
 	static {
 		fluidConsumptionRate.put("mobessence", 10);
 		fluidConsumptionRate.put("liquidessence", 20);
 		fluidConsumptionRate.put("ender", 40);
 	}
-	
+
 	protected short _radius;
 	protected boolean activated, unableToRequestTicket;
 	public boolean useAltPower;
@@ -59,25 +60,25 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 	protected int consumptionTicks;
 	protected int emptyTicks, prevEmpty;
 	protected int unactivatedTicks;
-	
+
 	public TileEntityChunkLoader()
 	{
 		super(Machine.ChunkLoader);
 		_radius = 0;
 		useAltPower = MFRConfig.enableConfigurableCLEnergy.getBoolean(false);
 	}
-	
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public GuiFactoryInventory getGui(InventoryPlayer inventoryPlayer)
 	{
 		return new GuiChunkLoader(getContainer(inventoryPlayer), this);
 	}
-	
+
 	@Override
 	public ContainerFactoryPowered getContainer(InventoryPlayer inventoryPlayer)
 	{
-		if (unableToRequestTicket && 
+		if (unableToRequestTicket &&
 				inventoryPlayer.player.getCommandSenderName().equals(_owner))
 		{
 			inventoryPlayer.player.addChatMessage(
@@ -85,13 +86,13 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 		}
 		return new ContainerChunkLoader(this, inventoryPlayer);
 	}
-	
+
 	@Override
 	protected FluidTankAdv[] createTanks()
 	{
 		return new FluidTankAdv[] {new FluidTankAdv(BUCKET_VOLUME * 10)};
 	}
-	
+
 	@Override
 	public void onChunkUnload()
 	{
@@ -102,7 +103,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 			ForgeChunkManager.releaseTicket(_ticket);
 		}
 	}
-	
+
 	public void setRadius(short r)
 	{
 		int maxR = 38;
@@ -135,7 +136,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 		}
 		return true;
 	}
-	
+
 	@Override
 	public void updateEntity()
 	{
@@ -162,7 +163,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 				unforceChunks();
 			return;
 		}
-		
+
 		if (!activated)
 		l: {
 			if (_ticket != null)
@@ -170,7 +171,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 				Set<ChunkCoordIntPair> chunks = _ticket.getChunkList();
 				if (chunks.size() == 0)
 					break l;
-				
+
 				unactivatedTicks = Math.min(_tanks[0].getCapacity() + 10, unactivatedTicks + 1);
 				if (consumptionTicks > 0)
 					consumptionTicks /= 10;
@@ -189,7 +190,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 							break l;
 					}
 				}
-				
+
 				for (ChunkCoordIntPair c : chunks)
 					ForgeChunkManager.unforceChunk(_ticket, c);
 			}
@@ -209,30 +210,30 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 				_ticket.getModData().setInteger("X", xCoord);
 				_ticket.getModData().setInteger("Y", yCoord);
 				_ticket.getModData().setInteger("Z", zCoord);
-				
+
 			}
 			forceChunks();
 		}
-		
+
 		if (prevEmpty != emptyTicks)
 		{
 			prevEmpty = emptyTicks;
 			onFactoryInventoryChanged();
 		}
-		
+
 		super.setIsActive(activated);
 	}
-	
+
 	protected void unforceChunks()
 	{
 		Set<ChunkCoordIntPair> chunks = _ticket.getChunkList();
 		if (chunks.size() == 0)
 			return;
-		
+
 		for (ChunkCoordIntPair c : chunks)
 			ForgeChunkManager.unforceChunk(_ticket, c);
 	}
-	
+
 	protected void forceChunks()
 	{
 		if (_ticket == null)
@@ -262,7 +263,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 				}
 		}
 	}
-	
+
 	@Override
 	protected void onFactoryInventoryChanged()
 	{
@@ -338,30 +339,41 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 		}
 		return false;
 	}
-	
+
+	@Override
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		tag.setShort("radius", _radius);
+	}
+
+	@Override
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		setRadius(tag.getShort("radius"));
+	}
+
 	@Override
 	public void writeToNBT(NBTTagCompound tag)
 	{
 		super.writeToNBT(tag);
-		
+
 		tag.setShort("radius", _radius);
 		tag.setInteger("empty", emptyTicks);
 		tag.setInteger("inactive", unactivatedTicks);
 		tag.setInteger("consumed", consumptionTicks);
 	}
-	
+
 	@Override
 	public void readFromNBT(NBTTagCompound tag)
 	{
 		super.readFromNBT(tag);
-		
-		_radius = tag.getShort("radius");
+
 		emptyTicks = tag.getInteger("empty");
 		unactivatedTicks = tag.getInteger("inactive");
 		consumptionTicks = tag.getInteger("consumed");
 		onFactoryInventoryChanged();
 	}
-	
+
 	protected boolean isFluidFuel(FluidStack fuel)
 	{
 		String name = getFluidName(fuel);
@@ -369,7 +381,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 			return false;
 		return fluidConsumptionRate.containsKey(name);
 	}
-	
+
 	@Override
 	public int fill(ForgeDirection from, FluidStack resource, boolean doFill)
 	{
@@ -379,25 +391,25 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 					return _tank.fill(resource, doFill);
 		return 0;
 	}
-	
+
 	@Override
 	public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain)
 	{
 		return drain(maxDrain, doDrain);
 	}
-	
+
 	@Override
 	public FluidStack drain(ForgeDirection from, FluidStack resource, boolean doDrain)
 	{
 		return drain(resource, doDrain);
 	}
-	
+
 	@Override
 	public boolean allowBucketFill(ItemStack stack)
 	{
 		return !unableToRequestTicket;
 	}
-	
+
 	@Override
 	public boolean allowBucketDrain(ItemStack stack)
 	{
@@ -415,7 +427,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 	{
 		return unableToRequestTicket;
 	}
-	
+
 	protected String getFluidName(FluidStack fluid)
 	{
 		if (fluid == null || fluid.getFluid() == null)

resources/assets/minefactoryreloaded/language/en_US.lang
@@ -468,6 +468,7 @@ tip.info.mfr.safarinet.persistent = Released creatures don't despawn normally
 tip.info.mfr.safarinet.nametag = Released creatures will always display their nametag
 tip.info.mfr.safarinet.mystery = It is a mystery
 
+tip.info.mfr.memorycard.programmed = Programmed %d circuits
 tip.info.mfr.memorycard.wipe = Place in crafting grid to wipe
 
 tip.info.mfr.generator.produces = Produces RF only.

src/powercrystals/minefactoryreloaded/item/tool/ItemRedNetMemoryCard.java
@@ -1,5 +1,6 @@
 package powercrystals.minefactoryreloaded.item.tool;
 
+import cofh.api.tileentity.IPortableData;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -21,39 +22,73 @@ public class ItemRedNetMemoryCard extends ItemFactory {
 	@Override
 	public void addInfo(ItemStack stack, EntityPlayer player, List<String> infoList, boolean advancedTooltips) {
 		super.addInfo(stack, player, infoList, advancedTooltips);
-		if (stack.getTagCompound() != null) {
-			infoList.add("Programmed, " + stack.getTagCompound().getTagList("circuits", 10).tagCount() + " circuits");
-			// TODO: localize ^
+		NBTTagCompound tag = stack.getTagCompound();
+		if (tag != null) {
+			l: {
+				if (tag.hasKey("Type") && !tag.getString("Type").equals("tile.mfr.rednet.logic.name"))
+					break l;
+				if (tag.hasKey("circuits", 10)) {
+					int c = stack.getTagCompound().getTagList("circuits", 10).tagCount();
+					infoList.add(MFRUtil.localize("tip.info.mfr.memorycard.programmed", c));
+				}
+			}
 			infoList.add(MFRUtil.localize("tip.info.mfr.memorycard.wipe", true));
 		}
 	}
 
 	@Override
-	public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World world, int x, int y, int z, int side,
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
 			float xOffset, float yOffset, float zOffset) {
 		if (world.isRemote) {
 			return true;
 		}
 
 		TileEntity te = world.getTileEntity(x, y, z);
-		if (te instanceof TileEntityRedNetLogic) {
-			if (itemstack.getTagCompound() == null) {
-				NBTTagCompound tag = new NBTTagCompound();
-				te.writeToNBT(tag);
-				itemstack.setTagCompound(tag);
-				player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.uploaded"));
-			} else {
-				int circuitCount = itemstack.getTagCompound().getTagList("circuits", 10).tagCount();
-				if (circuitCount > ((TileEntityRedNetLogic)te).getCircuitCount()) {
-					player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.error"));
-				} else {
-					((TileEntityRedNetLogic)te).readCircuitsOnly(itemstack.getTagCompound());
-					player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.downloaded"));
+		NBTTagCompound tag = stack.getTagCompound();
+		boolean read = tag == null || !tag.hasKey("Type"), special = false;
+		if (tag == null)
+			stack.setTagCompound(tag = new NBTTagCompound());
+		else if (read != tag.hasKey("circuits", 10)) {
+			special = true;
+			read = false;
+		}
+
+		l: if (!special && te instanceof IPortableData) {
+			if (read) {
+				NBTTagCompound tag2 = (NBTTagCompound) tag.copy();
+				((IPortableData) te).writePortableData(player, tag2);
+				if (!tag2.equals(tag)) {
+					tag = tag2;
+					tag.setString("Type", ((IPortableData) te).getDataType());
 				}
+			} else {
+				if (tag.getString("Type").equals(((IPortableData) te).getDataType())) {
+					((IPortableData) te).readPortableData(player, tag);
+				} else
+					break l;
 			}
+			stack.setTagCompound(tag);
 
 			return true;
 		}
+		else if (te instanceof TileEntityRedNetLogic) {
+			if (read) {
+				te.writeToNBT(tag);
+				player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.uploaded"));
+			} else if (special) {
+				int circuitCount = tag.getTagList("circuits", 10).tagCount();
+				if (circuitCount > ((TileEntityRedNetLogic)te).getCircuitCount()) {
+					player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.error"));
+				} else {
+					((TileEntityRedNetLogic)te).readCircuitsOnly(tag);
+					player.addChatMessage(new ChatComponentTranslation("chat.info.mfr.rednet.memorycard.downloaded"));
+				}
+			}
+			stack.setTagCompound(tag);
+
+			return true;
+		}
+
 		return false;
 	}
 

src/powercrystals/minefactoryreloaded/tile/base/TileEntityFactory.java
@@ -4,12 +4,14 @@ import buildcraft.api.transport.IPipeConnection;
 import buildcraft.api.transport.IPipeTile.PipeType;
 
 import cofh.api.inventory.IInventoryConnection;
+import cofh.api.tileentity.IPortableData;
 import cofh.asm.relauncher.Strippable;
 import cofh.lib.util.position.IRotateableTile;
 import com.google.common.base.Strings;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
@@ -29,8 +31,7 @@ import powercrystals.minefactoryreloaded.setup.Machine;
 
 @Strippable("buildcraft.api.transport.IPipeConnection")
 public abstract class TileEntityFactory extends TileEntityBase
-									 implements IRotateableTile, IPipeConnection,
-												IHarvestAreaContainer, IInventoryConnection
+implements IRotateableTile, IInventoryConnection, IPortableData, IHarvestAreaContainer, IPipeConnection
 {
 	// first index is rotation, second is side
 	private static final int[][] _textureSelection = new int[][]
@@ -347,12 +348,18 @@ public abstract class TileEntityFactory extends TileEntityBase
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound tag)
-	{
-		super.readFromNBT(tag);
-		int rotation = tag.getInteger("rotation");
-		rotateDirectlyTo(rotation);
-		_owner = tag.getString("owner");
+	public String getDataType() {
+		return _machine.getInternalName() + ".name";
+	}
+
+	@Override
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+	}
+
+	@Override
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
 	}
 
 	@Override
@@ -360,6 +367,15 @@ public abstract class TileEntityFactory extends TileEntityBase
 	{
 		super.writeToNBT(tag);
 		tag.setInteger("rotation", getDirectionFacing().ordinal());
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		super.readFromNBT(tag);
+		int rotation = tag.getInteger("rotation");
+		rotateDirectlyTo(rotation);
+		_owner = tag.getString("owner");
 	}
 
 	@Override

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoAnvil.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -444,6 +445,18 @@ public class TileEntityAutoAnvil extends TileEntityFactoryPowered implements ITa
 
 			return outputItem;
 		}
+	}
+
+	@Override
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		tag.setBoolean("repairOnly", repairOnly);
+	}
+
+	@Override
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		setRepairOnly(tag.getBoolean("repairOnly"));
 	}
 
 	@Override

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoDisenchanter.java
@@ -3,6 +3,7 @@ package powercrystals.minefactoryreloaded.tile.machine;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -188,6 +189,18 @@ public class TileEntityAutoDisenchanter extends TileEntityFactoryPowered
 	public int getIdleTicksMax()
 	{
 		return 1;
+	}
+
+	@Override
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		tag.setBoolean("repeatDisenchant", _repeatDisenchant);
+	}
+
+	@Override
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		_repeatDisenchant = tag.getBoolean("repeatDisenchant");
 	}
 
 	@Override

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoEnchanter.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Random;
 
 import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -258,17 +259,29 @@ public class TileEntityAutoEnchanter extends TileEntityFactoryPowered implements
 	}
 
 	@Override
-	public void writeItemNBT(NBTTagCompound nbttagcompound)
-	{
-		super.writeItemNBT(nbttagcompound);
-		nbttagcompound.setInteger("targetLevel", _targetLevel);
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		tag.setInteger("targetLevel", _targetLevel);
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound nbttagcompound)
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		setTargetLevel(tag.getInteger("targetLevel"));
+	}
+
+	@Override
+	public void writeItemNBT(NBTTagCompound tag)
 	{
-		super.readFromNBT(nbttagcompound);
-		_targetLevel = nbttagcompound.getInteger("targetLevel");
+		super.writeItemNBT(tag);
+		tag.setInteger("targetLevel", _targetLevel);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		super.readFromNBT(tag);
+		_targetLevel = tag.getInteger("targetLevel");
 	}
 
 	@Override

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoSpawner.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityWaterMob;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -310,17 +311,29 @@ public class TileEntityAutoSpawner extends TileEntityFactoryPowered implements I
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound nbttagcompound)
-	{
-		super.readFromNBT(nbttagcompound);
-		_spawnExact = nbttagcompound.getBoolean("spawnExact");
+	public void writePortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		tag.setBoolean("spawnExact", _spawnExact);
 	}
 
 	@Override
-	public void writeItemNBT(NBTTagCompound nbttagcompound)
+	public void readPortableData(EntityPlayer player, NBTTagCompound tag) {
+
+		setSpawnExact(tag.getBoolean("spawnExact"));
+	}
+
+	@Override
+	public void writeItemNBT(NBTTagCompound tag)
 	{
-		super.writeItemNBT(nbttagcompound);
-		nbttagcompound.setBoolean("spawnExact", _spawnExact);
+		super.writeItemNBT(tag);
+		tag.setBoolean("spawnExact", _spawnExact);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		super.readFromNBT(tag);
+		_spawnExact = tag.getBoolean("spawnExact");
 	}
 
 	@Override

commit 884601f15e81a4cee96c31f3e3926cc9033e285c

Fix #357
Bug introduced by 8409f239593d58d6708eba76e67f6de353039fc8

src/powercrystals/minefactoryreloaded/tile/machine/TileEntityChunkLoader.java
@@ -368,6 +368,7 @@ public class TileEntityChunkLoader extends TileEntityFactoryPowered implements I
 	{
 		super.readFromNBT(tag);
 
+		setRadius(tag.getShort("radius"));
 		emptyTicks = tag.getInteger("empty");
 		unactivatedTicks = tag.getInteger("inactive");
 		consumptionTicks = tag.getInteger("consumed");