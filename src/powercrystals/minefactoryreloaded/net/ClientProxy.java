04ef7ad7a0b65af2a42b1c061c4dc9fc20f416fb
skyboy <skyboy026@gmail.com>
2015-02-23 09:06:35 -0500

    Stencil enabling in core

--- src/powercrystals/minefactoryreloaded/net/ClientProxy.java
+++ src/powercrystals/minefactoryreloaded/net/ClientProxy.java
@@ -1,11 +1,6 @@
 package powercrystals.minefactoryreloaded.net;
 
-import cpw.mods.fml.relauncher.ReflectionHelper;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraftforge.client.ForgeHooksClient;
 
 import powercrystals.minefactoryreloaded.MineFactoryReloadedClient;
 
@@ -16,15 +11,6 @@ public class ClientProxy extends CommonProxy
 	{
 		super.init();
 		MineFactoryReloadedClient.init();
-		if (!Boolean.parseBoolean(System.getProperty("forge.forceDisplayStencil", "false"))) {
-			try {
-				ReflectionHelper.findField(ForgeHooksClient.class, "stencilBits").setInt(null, 2);
-				Framebuffer b = Minecraft.getMinecraft().getFramebuffer();
-				b.createBindFramebuffer(b.framebufferWidth, b.framebufferHeight);
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 	@Override
