8b97adf75d65aa3190e4841c0eb28210d9bcadab
skyboy <skyboy026@gmail.com>
2015-02-25 13:19:19 -0500

    Localize DSU stored text

--- src/powercrystals/minefactoryreloaded/gui/client/GuiDeepStorageUnit.java
+++ src/powercrystals/minefactoryreloaded/gui/client/GuiDeepStorageUnit.java
@@ -1,39 +1,39 @@
 package powercrystals.minefactoryreloaded.gui.client;
 
+import powercrystals.minefactoryreloaded.core.MFRUtil;
 import powercrystals.minefactoryreloaded.gui.container.ContainerFactoryInventory;
 import powercrystals.minefactoryreloaded.tile.machine.TileEntityDeepStorageUnit;
 
 public class GuiDeepStorageUnit extends GuiFactoryInventory
 {
 	private TileEntityDeepStorageUnit _dsu;
-	
+
 	public GuiDeepStorageUnit(ContainerFactoryInventory container, TileEntityDeepStorageUnit dsu)
 	{
 		super(container, dsu);
 		_dsu = dsu;
 		ySize = 205;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initGui()
 	{
 		super.initGui();
 	}
-	
+
 	@Override
 	public void updateScreen()
 	{
 		super.updateScreen();
 	}
-	
+
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
 	{
 		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
-		
-		// TODO: localize
-		fontRendererObj.drawString("Stored:", 110, 70, 4210752);
-		fontRendererObj.drawString(((Integer)_dsu.getQuantity()).toString(), 110, 80, 4210752);
+
+		fontRendererObj.drawString(MFRUtil.localize("info.cofh.stored"), 110, 70, 4210752);
+		fontRendererObj.drawString(String.valueOf(_dsu.getQuantity()), 110, 80, 4210752);
 	}
 }
