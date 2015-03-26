37c30d4a8d2ba9c81a8bbe4d154c604e0489bd7a
skyboy <skyboy026@gmail.com>
2015-03-26 10:55:00 -0400

    Fix a missed "j"

---
+++ src/powercrystals/minefactoryreloaded/tile/transport/TileEntityDetCord.java
@@ -0,0 +1,14 @@
+package powercrystals.minefactoryreloaded.tile.transport;
+
+import net.minecraft.tileentity.TileEntity;
+
+
+public class TileEntityDetCord extends TileEntity {
+
+	@Override
+	public boolean canUpdate() {
+
+		return false;
+	}
+
+}
