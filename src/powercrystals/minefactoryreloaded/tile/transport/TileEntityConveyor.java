ca8e23aeab1c107d4e9fbe179ae0eba9f5f20465
skyboy <mr.wraith.06@gmail.com>
2015-08-15 14:16:05 -0400

    Fix #478

--- src/powercrystals/minefactoryreloaded/tile/transport/TileEntityConveyor.java
+++ src/powercrystals/minefactoryreloaded/tile/transport/TileEntityConveyor.java
@@ -29,22 +29,22 @@ public class TileEntityConveyor extends TileEntityBase
 			implements IRotateableTile, ISidedInventory, IPipeConnection, IInventoryConnection
 {
 	private int _dye = -1;
-	
+
 	private boolean _rednetReversed = false;
 	private boolean _isReversed = false;
 	private boolean _gateReversed = false;
-	
+
 	private boolean _redNetAllowsActive = true;
 	private boolean _gateAllowsActive = true;
 	private boolean _conveyorActive = true;
-	
+
 	private boolean _isFast = false;
-	
+
 	public int getDyeColor()
 	{
 		return _dye;
 	}
-	
+
 	public void setDyeColor(int dye)
 	{
 		if(worldObj != null && !worldObj.isRemote && _dye != dye)
@@ -53,7 +53,7 @@ public class TileEntityConveyor extends TileEntityBase
 		}
 		_dye = dye;
 	}
-	
+
 	@Override
 	public Packet getDescriptionPacket()
 	{
@@ -64,7 +64,7 @@ public class TileEntityConveyor extends TileEntityBase
 		S35PacketUpdateTileEntity packet = new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 0, data);
 		return packet;
 	}
-	
+
 	@Override
 	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
 	{
@@ -73,13 +73,13 @@ public class TileEntityConveyor extends TileEntityBase
 		_conveyorActive = data.getBoolean("conveyorActive");
 		_isFast = data.getBoolean("isFast");
 	}
-	
+
 	@Override
 	public boolean shouldRefresh(Block oldID, Block newID, int oldMeta, int newMeta, World world, int x, int y, int z)
 	{
 		return oldID != newID;
 	}
-	
+
 	@Override
 	public void rotate(ForgeDirection axis)
 	{
@@ -115,7 +115,7 @@ public class TileEntityConveyor extends TileEntityBase
 			rotateTo(worldObj, xCoord, yCoord, zCoord, 1);
 		}
 		else
-		
+
 		if (md == 1)
 		{
 			if (isSideSolid(0, 1, NORTH))
@@ -147,7 +147,7 @@ public class TileEntityConveyor extends TileEntityBase
 			rotateTo(worldObj, xCoord, yCoord, zCoord, 2);
 		}
 		else
-		
+
 		if (md == 2)
 		{
 			if (isSideSolid(-1, 0, EAST))
@@ -179,7 +179,7 @@ public class TileEntityConveyor extends TileEntityBase
 			rotateTo(worldObj, xCoord, yCoord, zCoord, 3);
 		}
 		else
-		
+
 		if (md == 3)
 		{
 			if (isSideSolid(0, -1, SOUTH))
@@ -211,7 +211,7 @@ public class TileEntityConveyor extends TileEntityBase
 			rotateTo(worldObj, xCoord, yCoord, zCoord, 0);
 		}
 	}
-	
+
 	private boolean isSideSolid(int x, int z, ForgeDirection dir)
 	{
 		return worldObj.isSideSolid(xCoord + x, yCoord, zCoord + z, dir) &&
@@ -219,7 +219,7 @@ public class TileEntityConveyor extends TileEntityBase
 						!worldObj.isAirBlock(xCoord, yCoord + 1, zCoord)) ||
 							!worldObj.isSideSolid(xCoord - x, yCoord - 1, zCoord - z, UP));
 	}
-	
+
 	private void rotateTo(World world, int xCoord, int yCoord, int zCoord, int newmd)
 	{
 		world.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, newmd, 2);
@@ -236,19 +236,19 @@ public class TileEntityConveyor extends TileEntityBase
 	{
 		return true;
 	}
-	
+
 	@Override
 	public boolean canRotate(ForgeDirection axis)
 	{
 		return true;
 	}
-	
+
 	@Override
-	public ForgeDirection getDirectionFacing() 
+	public ForgeDirection getDirectionFacing()
 	{
 		return ForgeDirection.UNKNOWN;
 	}
-	
+
 	@Override
 	public boolean canUpdate()
 	{
@@ -264,12 +264,12 @@ public class TileEntityConveyor extends TileEntityBase
 	{
 		_isFast = fast;
 	}
-	
+
 	@Override
 	public void writeToNBT(NBTTagCompound tag)
 	{
 		super.writeToNBT(tag);
-		
+
 		tag.setInteger("dyeColor", _dye);
 		tag.setBoolean("isReversed", _isReversed);
 		tag.setBoolean("redNetActive", _conveyorActive);
@@ -278,12 +278,12 @@ public class TileEntityConveyor extends TileEntityBase
 		tag.setBoolean("gateReversed", _gateReversed);
 		tag.setBoolean("glowstone", _isFast);
 	}
-	
+
 	@Override
 	public void readFromNBT(NBTTagCompound tag)
 	{
 		super.readFromNBT(tag);
-		
+
 		if(tag.hasKey("dyeColor"))
 		{
 			_dye = tag.getInteger("dyeColor");
@@ -301,44 +301,48 @@ public class TileEntityConveyor extends TileEntityBase
 		_gateReversed = tag.getBoolean("gateReversed");
 		_isFast = tag.getBoolean("glowstone");
 	}
-	
+
 	//IInventory
 	@Override
 	public int getSizeInventory()
 	{
 		return 7;
 	}
-	
+
 	@Override
 	public ItemStack getStackInSlot(int slot)
 	{
 		return null;
 	}
-	
+
 	@Override
 	public ItemStack decrStackSize(int slot, int count)
 	{
 		return null;
 	}
-	
+
 	@Override
 	public ItemStack getStackInSlotOnClosing(int slot)
 	{
 		return null;
 	}
-	
+
 	@Override
 	public void setInventorySlotContents(int slot, ItemStack stack)
 	{
+		if (stack == null)
+		{
+			return;
+		}
 		int	horizDirection = worldObj.getBlockMetadata(xCoord, yCoord, zCoord) & 0x03;
-		
+
 		float dropOffsetX = 0.5F;
 		float dropOffsetY = 0.4F;
 		float dropOffsetZ = 0.5F;
 		double motionX = 0.0D;
 		double motionY = 0.0D;
 		double motionZ = 0.0D;
-		
+
 		//because of the setup, slot is also the ForgeDirection ordinal from which the item is being inserted
 		switch(slot)
 		{
@@ -349,7 +353,7 @@ public class TileEntityConveyor extends TileEntityBase
 			case 1: //UP
 				dropOffsetY = 0.8F;
 				motionY = -0.15D;
-				break;				
+				break;
 			case 2: //NORTH
 				dropOffsetZ = 0.2F;
 				motionZ = 0.15D;
@@ -368,7 +372,7 @@ public class TileEntityConveyor extends TileEntityBase
 				break;
 			case 6: //UNKNOWN
 		}
-		
+
 		if(horizDirection == 0)
 		{
 			motionX = 0.05D;
@@ -385,7 +389,7 @@ public class TileEntityConveyor extends TileEntityBase
 		{
 			motionZ = -0.05D;
 		}
-		
+
 		EntityItem entityitem = new EntityItem(worldObj, xCoord + dropOffsetX, yCoord + dropOffsetY, zCoord + dropOffsetZ, stack.copy());
 		entityitem.motionX = motionX;
 		entityitem.motionY = motionY;
@@ -393,36 +397,36 @@ public class TileEntityConveyor extends TileEntityBase
 		entityitem.delayBeforeCanPickup = 20;
 		worldObj.spawnEntityInWorld(entityitem);
 	}
-	
+
 	@Override
 	public String getInventoryName()
 	{
 		return "Conveyor Belt";
 	}
-	
+
 	@Override
 	public boolean hasCustomInventoryName()
 	{
 		return false;
 	}
-	
+
 	@Override
 	public int getInventoryStackLimit()
 	{
 		return 64;
 	}
-	
+
 	@Override
 	public void markDirty()
 	{
 	}
-	
+
 	@Override
 	public boolean isUseableByPlayer(EntityPlayer player)
 	{
 		return false;
 	}
-	
+
     @Override
 	public void openInventory()
     {
@@ -432,13 +436,13 @@ public class TileEntityConveyor extends TileEntityBase
 	public void closeInventory()
     {
     }
-    
+
     @Override
 	public boolean isItemValidForSlot(int slot, ItemStack stack)
     {
     	return _conveyorActive;
     }
-    
+
     //ISidedInventory
     @Override
 	public int[] getAccessibleSlotsFromSide(int sideOrdinal)
@@ -446,7 +450,7 @@ public class TileEntityConveyor extends TileEntityBase
     	int[] accessibleSlot = {sideOrdinal};
     	return accessibleSlot;
     }
-    
+
     /*
      * From above: returns true if the conveyor is not going uphill
      * For the NSEW sides: returns true if (conveyor is going uphill) || (!conveyor is facing in the 'from' direction)
@@ -478,13 +482,13 @@ public class TileEntityConveyor extends TileEntityBase
     		return true;
     	}
     }
-    
+
     @Override
 	public boolean canExtractItem(int slot, ItemStack stack, int side)
     {
     	return false;
     }
-	
+
 	// RedNet
 	public void onRedNetChanged(int value)
 	{
@@ -496,66 +500,66 @@ public class TileEntityConveyor extends TileEntityBase
 		setReversed(_gateReversed | (_rednetReversed = value < 0));
 		worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
 	}
-	
+
 	public void updateConveyorActive()
 	{
 		setConveyorActive(_gateAllowsActive & _redNetAllowsActive && !CoreUtils.isRedstonePowered(this));
 	}
-	
+
 	public boolean getConveyorActive()
 	{
 		return _conveyorActive;
 	}
-	
+
 	public void setConveyorActive(boolean conveyorActive)
 	{
 		boolean wasActive = _conveyorActive;
 		_conveyorActive = conveyorActive;
-		
+
 		if(wasActive ^ _conveyorActive)
 		{
 			worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
 		}
 	}
-	
+
 	public void setConveyerActiveFromGate(boolean conveyorActive)
 	{
 		boolean wasActive = _gateAllowsActive;
 		_gateAllowsActive = conveyorActive;
-		
+
 		if(wasActive ^ _gateAllowsActive)
 		{
 			updateConveyorActive();
 		}
 	}
-	
+
 	public boolean getConveyorReversed()
 	{
 		return _isReversed;
 	}
-	
+
 	private void setReversed(boolean isReversed)
 	{
 		boolean wasReversed = _isReversed;
 		_isReversed = isReversed;
-		
+
 		if(wasReversed ^ _isReversed)
 		{
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, getReversedMeta(worldObj.getBlockMetadata(xCoord, yCoord, zCoord)), 3);
 		}
 	}
-	
+
 	@SuppressWarnings("unused")
 	private void reverseConveyor()
 	{
 		setReversed(_rednetReversed | (_gateReversed = !_isReversed));
 	}
-	
+
 	private int getReversedMeta(int meta)
 	{
 		int directionComponent = ( meta + 2 ) % 4;
 		int slopeComponent;
-		
+
 		if(meta / 4 == 1)
 		{
 			slopeComponent = 2;
@@ -568,10 +572,10 @@ public class TileEntityConveyor extends TileEntityBase
 		{
 			slopeComponent = 0;
 		}
-		
+
 		return slopeComponent * 4 + directionComponent;
 	}
-	
+
 	@Override
 	public ConnectionType canConnectInventory(ForgeDirection from)
 	{
