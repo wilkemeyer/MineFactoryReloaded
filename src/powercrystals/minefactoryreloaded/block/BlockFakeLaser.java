9b20b724ab2dd2e0fb5cc0b43865e9cf0162e535
skyboy <mr.wraith.06@gmail.com>
2015-11-01 06:13:56 -0500

    Laser drill sets you on fire first

--- src/powercrystals/minefactoryreloaded/block/BlockFakeLaser.java
+++ src/powercrystals/minefactoryreloaded/block/BlockFakeLaser.java
@@ -22,13 +22,13 @@ import powercrystals.minefactoryreloaded.api.rednet.connectivity.IRedNetNoConnec
 import powercrystals.minefactoryreloaded.core.GrindingDamage;
 import powercrystals.minefactoryreloaded.tile.machine.TileEntityLaserDrill;
 
-public class BlockFakeLaser extends Block implements IRedNetNoConnection
-{
+public class BlockFakeLaser extends Block implements IRedNetNoConnection {
+
 	public static Material laser = new MaterialTransparent(MapColor.airColor);
 	private static GrindingDamage laserDamage = new GrindingDamage("mfr.laser");
 
-	public BlockFakeLaser()
-	{
+	public BlockFakeLaser() {
+
 		super(laser);
 		setHardness(-1);
 		setResistance(Float.POSITIVE_INFINITY);
@@ -37,111 +37,111 @@ public class BlockFakeLaser extends Block implements IRedNetNoConnection
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z)
-	{
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+
 		return null;
 	}
 
 	@Override
-	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
-	{
+	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+
 		if (world.isRemote)
 			return;
+
+		entity.setFire(15);
 		long t = entity.getEntityData().getLong("mfr:laserTime"), t2 = world.getTotalWorldTime();
 		t = t2 - t;
-		int d = t/20 >= 5 ? 1 : entity.getEntityData().getInteger("mfr:laserDamage") | 1;
-		if (t > 10 && entity.attackEntityFrom(laserDamage, d))
-		{
+		long d = t / 20 >= 5 ? 1 : entity.getEntityData().getLong("mfr:laserDamage") | 1;
+		d &= -1L >>> 1L;
+		if (t > 10 && entity.attackEntityFrom(laserDamage, d)) {
 			entity.getEntityData().setLong("mfr:laserTime", t2 + (d == 1 ? 20 : 0));
-			entity.getEntityData().setInteger("mfr:laserDamage", d * 2);
+			entity.getEntityData().setLong("mfr:laserDamage", d * 2);
 		}
-		entity.setFire(15);
 	}
 
 	@Override
-	public int getLightValue(IBlockAccess world, int x, int y, int z)
-	{
+	public int getLightValue(IBlockAccess world, int x, int y, int z) {
+
 		return 15;
 	}
 
 	@Override
-	public boolean isOpaqueCube()
-	{
+	public boolean isOpaqueCube() {
+
 		return false;
 	}
 
 	@Override
-	public boolean renderAsNormalBlock()
-	{
+	public boolean renderAsNormalBlock() {
+
 		return false;
 	}
 
 	@Override
-	public boolean isAir(IBlockAccess world, int x, int y, int z)
-	{
+	public boolean isAir(IBlockAccess world, int x, int y, int z) {
+
 		return true;
 	}
 
 	@Override
-	public void onBlockAdded(World world, int x, int y, int z)
-	{
+	public void onBlockAdded(World world, int x, int y, int z) {
+
 		world.scheduleBlockUpdate(x, y, z, this, 1);
 	}
 
 	@Override
-	public void onNeighborBlockChange(World world, int x, int y, int z, Block id)
-	{
+	public void onNeighborBlockChange(World world, int x, int y, int z, Block id) {
+
 		world.scheduleBlockUpdate(x, y, z, this, 1);
 	}
 
 	@Override
-	public void updateTick(World world, int x, int y, int z, Random rand)
-	{
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+
 		if (world.isRemote) return;
+
 		int meta = world.getBlockMetadata(x, y, z);
-		l: if (meta != 0)
-		{
+		l: if (meta != 0) {
 			ForgeDirection dir = ForgeDirection.getOrientation(meta - 1);
 			if (world.getBlock(x + dir.offsetX, y + dir.offsetY, z + dir.offsetZ).equals(this))
 				if (world.getBlockMetadata(x + dir.offsetX, y + dir.offsetY, z + dir.offsetZ) == meta)
 					return;
-				else break l;
+				else
+					break l;
 			TileEntity te = world.getTileEntity(x + dir.offsetX, y + dir.offsetY, z + dir.offsetZ);
-			if (te instanceof IFactoryLaserSource && ((IFactoryLaserSource)te).canFormBeamFrom(dir))
+			if (te instanceof IFactoryLaserSource && ((IFactoryLaserSource) te).canFormBeamFrom(dir))
 				return;
 			world.setBlockMetadataWithNotify(x, y, z, 0, 0);
 		}
 
 		Block upperId = world.getBlock(x, y + 1, z);
-		if (!upperId.equals(this) && !(world.getTileEntity(x, y + 1, z) instanceof TileEntityLaserDrill))
-		{
+		if (!upperId.equals(this) && !(world.getTileEntity(x, y + 1, z) instanceof TileEntityLaserDrill)) {
 			world.setBlockToAir(x, y, z);
 			return;
 		}
 
 		Block lowerId = world.getBlock(x, y - 1, z);
 		if ((!lowerId.equals(this) || world.getBlockMetadata(x, y - 1, z) != 0) &&
-				TileEntityLaserDrill.canReplaceBlock(lowerId, world, x, y - 1, z))
-		{
+				TileEntityLaserDrill.canReplaceBlock(lowerId, world, x, y - 1, z)) {
 			world.setBlock(x, y - 1, z, this);
 		}
 	}
 
 	@Override
-	public boolean canRenderInPass(int pass)
-	{
+	public boolean canRenderInPass(int pass) {
+
 		return false;
 	}
 
 	@Override
-	public int getRenderType()
-	{
+	public int getRenderType() {
+
 		return -1;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister ir)
-	{
+	public void registerBlockIcons(IIconRegister ir) {
+
 	}
 }
