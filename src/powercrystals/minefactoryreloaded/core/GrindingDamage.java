574ba9bf0870a3470578fc142aebde484e882611
skyboy <mr.wraith.06@gmail.com>
2015-11-01 06:14:15 -0500

    Grinding Damage immune to enchantments

--- src/powercrystals/minefactoryreloaded/core/GrindingDamage.java
+++ src/powercrystals/minefactoryreloaded/core/GrindingDamage.java
@@ -8,23 +8,25 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.util.StatCollector;
 
-public class GrindingDamage extends DamageSource
-{
+public class GrindingDamage extends DamageSource {
+
 	protected int _msgCount;
 	protected Random _rand;
-	public GrindingDamage()
-	{
+
+	public GrindingDamage() {
+
 		this(null, 1);
 	}
 
-	public GrindingDamage(String type)
-	{
+	public GrindingDamage(String type) {
+
 		this(type, 1);
 	}
 
-	public GrindingDamage(String type, int deathMessages)
-	{
+	public GrindingDamage(String type, int deathMessages) {
+
 		super(type == null ? "mfr.grinder" : type);
+		setDamageIsAbsolute();
 		setDamageBypassesArmor();
 		setDamageAllowedInCreativeMode();
 		_msgCount = Math.max(deathMessages, 1);
@@ -32,21 +34,19 @@ public class GrindingDamage extends DamageSource
 	}
 
 	@Override
-	public IChatComponent func_151519_b(EntityLivingBase entity)
-    {
-        EntityLivingBase entityliving1 = entity.func_94060_bK();
-        String s = "death.attack." + this.damageType;
-        if (_msgCount > 1)
-        {
-        	int msg = _rand.nextInt(_msgCount);
-        	if (msg != 0)
-        	{
-        		s += "." + msg;
-        	}
-        }
-        String s1 = s + ".player";
-        if (entityliving1 != null && StatCollector.canTranslate(s1))
-        	return new ChatComponentTranslation(s1, entity.getCommandSenderName(), entityliving1.getCommandSenderName());
-        return new ChatComponentTranslation(s, entity.getCommandSenderName());
-    }
+	public IChatComponent func_151519_b(EntityLivingBase entity) {
+
+		EntityLivingBase entityliving1 = entity.func_94060_bK();
+		String s = "death.attack." + this.damageType;
+		if (_msgCount > 1) {
+			int msg = _rand.nextInt(_msgCount);
+			if (msg != 0) {
+				s += "." + msg;
+			}
+		}
+		String s1 = s + ".player";
+		if (entityliving1 != null && StatCollector.canTranslate(s1))
+			return new ChatComponentTranslation(s1, entity.getCommandSenderName(), entityliving1.getCommandSenderName());
+		return new ChatComponentTranslation(s, entity.getCommandSenderName());
+	}
 }
