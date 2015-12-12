95988ca904ef3444ac94233664fd535d8aa9f745
skyboy <mr.wraith.06@gmail.com>
2015-12-12 02:02:32 -0500

    Laser focus slots no longer accept ceramic dye

--- src/powercrystals/minefactoryreloaded/gui/slot/SlotAcceptLaserFocus.java
+++ src/powercrystals/minefactoryreloaded/gui/slot/SlotAcceptLaserFocus.java
@@ -8,26 +8,28 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
-import powercrystals.minefactoryreloaded.item.base.ItemFactoryColored;
+import powercrystals.minefactoryreloaded.setup.MFRThings;
 
-public class SlotAcceptLaserFocus extends Slot
-{
+public class SlotAcceptLaserFocus extends Slot {
+
 	public static IIcon background;
-	public SlotAcceptLaserFocus(IInventory inv, int index, int x, int y)
-	{
+
+	public SlotAcceptLaserFocus(IInventory inv, int index, int x, int y) {
+
 		super(inv, index, x, y);
 	}
 
 	@Override
-	public boolean isItemValid(ItemStack stack)
-	{
-		return stack != null && stack.getItem() instanceof ItemFactoryColored;
+	public boolean isItemValid(ItemStack stack) {
+
+		return stack != null && stack.getItem().equals(MFRThings.laserFocusItem);
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
-    public IIcon getBackgroundIconIndex()
-    {
-        return background;
-    }
+	public IIcon getBackgroundIconIndex() {
+
+		return background;
+	}
+
 }
