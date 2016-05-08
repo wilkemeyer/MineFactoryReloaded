01bd3edd9ded25722457cf2b331e3ac284d03b0e
skyboy <mr.wraith.06@gmail.com>
2016-05-08 16:00:47 -0400

    Smooth white/black stone drop cobble versions

--- src/powercrystals/minefactoryreloaded/block/decor/BlockDecorativeStone.java
+++ src/powercrystals/minefactoryreloaded/block/decor/BlockDecorativeStone.java
@@ -3,6 +3,7 @@ package powercrystals.minefactoryreloaded.block.decor;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.block.Block;
@@ -11,21 +12,23 @@ import net.minecraft.block.BlockSand;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityFallingBlock;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import powercrystals.minefactoryreloaded.block.BlockFactory;
 
-public class BlockDecorativeStone extends BlockFactory
-{
-	public static final String[] _names = new String [] { "black.smooth", "white.smooth", "black.cobble",
-		"white.cobble", "black.brick.large", "white.brick.large", "black.brick.small",
-		"white.brick.small", "black.gravel", "white.gravel", "black.paved", "white.paved" };
+public class BlockDecorativeStone extends BlockFactory {
+
+	public static final String[] _names = new String[] { "black.smooth", "white.smooth", "black.cobble",
+			"white.cobble", "black.brick.large", "white.brick.large", "black.brick.small",
+			"white.brick.small", "black.gravel", "white.gravel", "black.paved", "white.paved" };
 	private IIcon[] _icons = new IIcon[_names.length];
 
-	public BlockDecorativeStone()
-	{
+	public BlockDecorativeStone() {
+
 		super(Material.rock);
 		setHardness(2.0F);
 		setResistance(10.0F);
@@ -34,112 +37,97 @@ public class BlockDecorativeStone extends BlockFactory
 		providesPower = false;
 	}
 
-	@SideOnly(Side.CLIENT)
 	@Override
-	public void registerBlockIcons(IIconRegister ir)
-	{
-		for(int i = 0; i < _icons.length; i++)
-		{
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister ir) {
+
+		for (int i = 0; i < _icons.length; i++) {
 			_icons[i] = ir.registerIcon("minefactoryreloaded:" + getUnlocalizedName() + "." + _names[i]);
 		}
 	}
 
 	@Override
-	public IIcon getIcon(int side, int meta)
-	{
+	public int damageDropped(int meta) {
+
+		if (meta == 0 | meta == 1) {
+			meta += 2; // smooth -> cobble
+		}
+		return meta;
+	}
+
+	@Override
+	public ArrayList<ItemStack> dismantleBlock(EntityPlayer player, World world, int x, int y, int z, boolean returnBlock) {
+
+		ArrayList<ItemStack> list = new ArrayList<ItemStack>();
+		int meta = world.getBlockMetadata(x, y, z);
+		list.add(new ItemStack(getItemDropped(meta, world.rand, 0), quantityDropped(world.rand), meta)); // persist metadata
+
+		world.setBlockToAir(x, y, z);
+		if (!returnBlock)
+			for (ItemStack item : list)
+				dropBlockAsItem(world, x, y, z, item);
+		return list;
+	}
+
+	@Override
+	public IIcon getIcon(int side, int meta) {
+
 		return _icons[Math.min(meta, _icons.length)];
 	}
 
 	@Override
-	public void onBlockAdded(World par1World, int par2, int par3, int par4)
-	{
-		par1World.scheduleBlockUpdate(par2, par3, par4, this, this.tickRate(par1World));
+	public void onBlockAdded(World world, int x, int y, int z) {
+
+		world.scheduleBlockUpdate(x, y, z, this, tickRate(world));
 	}
 
 	@Override
-	public void onNeighborBlockChange(World par1World, int par2, int par3, int par4, Block par5)
-	{
-		par1World.scheduleBlockUpdate(par2, par3, par4, this, this.tickRate(par1World));
+	public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+
+		world.scheduleBlockUpdate(x, y, z, this, tickRate(world));
 	}
 
 	@Override
-	public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
-	{
-		if (!par1World.isRemote)
-		{
-			this.tryToFall(par1World, par2, par3, par4);
+	public void updateTick(World world, int x, int y, int z, Random random) {
+
+		if (!world.isRemote) {
+			tryToFall(world, x, y, z);
 		}
 	}
 
-	private void tryToFall(World par1World, int par2, int par3, int par4)
-	{
-		int meta = par1World.getBlockMetadata(par2, par3, par4);
+	private void tryToFall(World world, int x, int y, int z) {
+
+		int meta = world.getBlockMetadata(x, y, z);
 		if (meta != 8 & meta != 9)
 			return;
-		if (BlockFalling.func_149831_e(par1World, par2, par3 - 1, par4) && par3 >= 0)
-		{
+		if (BlockFalling.func_149831_e(world, x, y - 1, z) && y >= 0) {
 			byte b0 = 32;
 
-			if (!BlockSand.fallInstantly && par1World.checkChunksExist(par2 - b0, par3 - b0, par4 - b0, par2 + b0, par3 + b0, par4 + b0))
-			{
-				if (!par1World.isRemote)
-				{
-					EntityFallingBlock entityfallingsand = new EntityFallingBlock(par1World, par2 + 0.5d, par3 + 0.5d, par4 + 0.5d, this, meta);
-					par1World.spawnEntityInWorld(entityfallingsand);
+			if (!BlockSand.fallInstantly && world.checkChunksExist(x - b0, y - b0, z - b0, x + b0, y + b0, z + b0)) {
+				if (!world.isRemote) {
+					EntityFallingBlock entityfallingsand = new EntityFallingBlock(world, x + 0.5d, y + 0.5d, z + 0.5d, this, meta);
+					world.spawnEntityInWorld(entityfallingsand);
 				}
-			}
-			else
-			{
-				par1World.setBlockToAir(par2, par3, par4);
+			} else {
+				world.setBlockToAir(x, y, z);
 
-				while (BlockFalling.func_149831_e(par1World, par2, par3 - 1, par4) && par3 > 0)
-				{
-					--par3;
+				while (BlockFalling.func_149831_e(world, x, y - 1, z) && y > 0) {
+					--y;
 				}
 
-				if (par3 > 0)
-				{
-					par1World.setBlock(par2, par3, par4, this, meta, 3);
+				if (y > 0) {
+					world.setBlock(x, y, z, this, meta, 3);
 				}
 			}
 		}
 	}
 
 	@Override
-	public int tickRate(World par1World)
-	{
+	public int tickRate(World par1World) {
+
 		return 2;
 	}
 
-	/* TODO: step sounds for gravel?
-	protected void updateStepSound(int meta)
-	{
-		switch(meta)
-		{
-		case 8:
-		case 9:
-			setStepSound(soundGravelFootstep);
-			break;
-		default:
-			setStepSound(soundStoneFootstep);
-		}
-	}
+	// TODO: step sounds require forge hook
 
-	@Override
-	public void onPostBlockPlaced(World world, int x, int y, int z, int meta)
-	{
-		updateStepSound(meta);
-	}
-
-	@Override
-	public void onFallenUpon(World world, int x, int y, int z, Entity entity, float distance)
-	{
-		updateStepSound(world.getBlockMetadata(x, y, z));
-	}
-
-	public void onEntityWalking(World par1World, int par2, int par3, int par4, Entity par5Entity)
-	{
-		// called after entity plays step sound
-	}
-	//*/
 }
