99308b88b13e0e7a83eb3387377201ac091dece5
skyboy <mr.wraith.06@gmail.com>
2016-05-09 15:48:28 -0400

    Move fluid textures

--- src/powercrystals/minefactoryreloaded/block/fluid/BlockFactoryFluid.java
+++ src/powercrystals/minefactoryreloaded/block/fluid/BlockFactoryFluid.java
@@ -35,12 +35,11 @@ import powercrystals.minefactoryreloaded.MFRRegistry;
 import powercrystals.minefactoryreloaded.api.rednet.connectivity.IRedNetDecorative;
 import powercrystals.minefactoryreloaded.setup.MFRThings;
 
-public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDecorative
-{ // TODO: convert to BlockFluidFinite
-	private static DamageSource steam = new DamageSource("steam");
-	{
-			steam.setDamageBypassesArmor().setFireDamage().setDifficultyScaled();
-			steam.setDamageIsAbsolute();
+public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDecorative { // TODO: convert to BlockFluidFinite
+
+	private static DamageSource steam = new DamageSource("steam"); {
+		steam.setDamageBypassesArmor().setFireDamage().setDifficultyScaled();
+		steam.setDamageIsAbsolute();
 	}
 	public static final Material material = new MaterialLiquid(MapColor.waterColor);
 
@@ -51,21 +50,22 @@ public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDeco
 	@SideOnly(Side.CLIENT)
 	public int color;
 	protected String fluidName;
-	private static Fluid ensureFluid(String name)
-	{
+
+	private static Fluid ensureFluid(String name) {
+
 		Fluid fluid = FluidRegistry.getFluid(name);
 		if (fluid.canBePlacedInWorld())
 			ReflectionHelper.setPrivateValue(Fluid.class, fluid, null, "block");
 		return fluid;
 	}
 
-	public BlockFactoryFluid(String liquidName)
-	{
+	public BlockFactoryFluid(String liquidName) {
+
 		this(liquidName, Material.water);
 	}
 
-	public BlockFactoryFluid(String liquidName, Material material)
-	{
+	public BlockFactoryFluid(String liquidName, Material material) {
+
 		super(ensureFluid(liquidName), material, liquidName);
 		setBlockName("mfr." + liquidName + ".still");
 		setHardness(100.0F);
@@ -75,54 +75,46 @@ public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDeco
 	}
 
 	@Override
-	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
-	{
+	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+
 		super.onEntityCollidedWithBlock(world, x, y, z, entity);
 
-		if (entity instanceof EntityLivingBase)
-		{
+		if (entity instanceof EntityLivingBase) {
 			NBTTagCompound data = entity.getEntityData();
-			if (world.isRemote || data.getLong("mfr:fluidTimer" + fluidName) > world.getTotalWorldTime())
-			{
+			if (world.isRemote || data.getLong("mfr:fluidTimer" + fluidName) > world.getTotalWorldTime()) {
 				return;
 			}
 			data.setLong("mfr:fluidTimer" + fluidName, world.getTotalWorldTime() + 40);
 
-			EntityLivingBase ent = (EntityLivingBase)entity;
-			if (this == MFRThings.milkLiquid)
-			{
+			EntityLivingBase ent = (EntityLivingBase) entity;
+			if (this == MFRThings.milkLiquid) {
 				ent.addPotionEffect(new PotionEffect(Potion.digSpeed.id, 6 * 20, 0, true));
 			}
-			else if (this == MFRThings.sludgeLiquid)
-			{
+			else if (this == MFRThings.sludgeLiquid) {
 				ent.addPotionEffect(new PotionEffect(Potion.wither.id, 12 * 20, 0));
 				ent.addPotionEffect(new PotionEffect(Potion.weakness.id, 12 * 20, 0, true));
 				ent.addPotionEffect(new PotionEffect(Potion.confusion.id, 16 * 20, 0, true));
 			}
-			else if (this == MFRThings.sewageLiquid)
-			{
+			else if (this == MFRThings.sewageLiquid) {
 				ent.addPotionEffect(new PotionEffect(Potion.hunger.id, 12 * 20, 0, true));
 				ent.addPotionEffect(new PotionEffect(Potion.poison.id, 12 * 20, 0));
 				ent.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 12 * 20, 0, true));
 			}
-			else if (this == MFRThings.essenceLiquid)
-			{
+			else if (this == MFRThings.essenceLiquid) {
 				ent.addPotionEffect(new PotionEffect(Potion.nightVision.id, 60 * 20, 0, true));
 			}
-			else if (this == MFRThings.biofuelLiquid)
-			{
+			else if (this == MFRThings.biofuelLiquid) {
 				ent.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 12 * 20, 0, true));
 			}
-			else if (this == MFRThings.steamFluid)
-			{
+			else if (this == MFRThings.steamFluid) {
 				ent.attackEntityFrom(steam, 6);
 			}
 		}
 	}
 
 	@Override
-	public int getLightValue(IBlockAccess world, int x, int y, int z)
-	{
+	public int getLightValue(IBlockAccess world, int x, int y, int z) {
+
 		int light = super.getLightValue(world, x, y, z);
 		if (maxScaledLight != 0)
 			light = Math.max(light, 2);
@@ -130,54 +122,47 @@ public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDeco
 	}
 
 	@Override
-	public void onBlockAdded(World world, int x, int y, int z)
-	{
+	public void onBlockAdded(World world, int x, int y, int z) {
+
 		super.onBlockAdded(world, x, y, z);
 		checkCanStay(world, x, y, z, world.rand);
 	}
 
 	@Override
-	public void updateTick(World world, int x, int y, int z, Random rand)
-	{
+	public void updateTick(World world, int x, int y, int z, Random rand) {
+
 		checkCanStay(world, x, y, z, rand);
 		super.updateTick(world, x, y, z, rand);
 	}
 
-	protected void checkCanStay(World world, int x, int y, int z, Random rand)
-	{
+	protected void checkCanStay(World world, int x, int y, int z, Random rand) {
+
 		/*BiomeGenBase biome = world.getBiomeGenForCoords(x, z);
 		l: if (biome != null && biome.getFloatTemperature() > 1.9f)//*/
 		l: if (world.provider.isHellWorld)
 		{
-			if (!isSourceBlock(world, x, y, z))
-			{
+			if (!isSourceBlock(world, x, y, z)) {
 				if (world.setBlockToAir(x, y, z))
 					return;
 				break l;
 			}
 			ItemStack drop = null;
 			Block block = Blocks.air;
-			if (this == MFRThings.milkLiquid)
-			{
+			if (this == MFRThings.milkLiquid) {
 				if (rand.nextInt(50) == 0)
 					drop = new ItemStack(Items.dye, rand.nextInt(2), 15);
 			}
-			else if (this == MFRThings.sludgeLiquid)
-			{
-				drop = ((WeightedRandomItemStack)WeightedRandom.
+			else if (this == MFRThings.sludgeLiquid) {
+				drop = ((WeightedRandomItemStack) WeightedRandom.
 						getRandomItem(rand, MFRRegistry.getSludgeDrops())).getStack();
 			}
-			else if (this == MFRThings.sewageLiquid)
-			{
+			else if (this == MFRThings.sewageLiquid) {
 				drop = new ItemStack(MFRThings.fertilizerItem, 1 + rand.nextInt(2));
 			}
-			else if (this == MFRThings.essenceLiquid)
-			{
-				if (world.setBlockToAir(x, y, z))
-				{
+			else if (this == MFRThings.essenceLiquid) {
+				if (world.setBlockToAir(x, y, z)) {
 					int i = rand.nextInt(5) + 10;
-					while (i > 0)
-					{
+					while (i > 0) {
 						int j = EntityXPOrb.getXPSplit(i);
 						i -= j;
 						world.spawnEntityInWorld(new EntityXPOrb(world,
@@ -188,40 +173,33 @@ public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDeco
 				}
 				break l;
 			}
-			else if (this == MFRThings.meatLiquid)
-			{
+			else if (this == MFRThings.meatLiquid) {
 				if (rand.nextInt(5) != 0)
 					drop = new ItemStack(MFRThings.meatIngotRawItem, rand.nextInt(2));
 				else
 					drop = new ItemStack(MFRThings.meatIngotCookedItem, rand.nextInt(2));
 			}
-			else if (this == MFRThings.pinkSlimeLiquid)
-			{
+			else if (this == MFRThings.pinkSlimeLiquid) {
 				if (rand.nextBoolean())
 					drop = new ItemStack(MFRThings.pinkSlimeItem, rand.nextInt(3));
+				else if (rand.nextInt(5) != 0)
+					drop = new ItemStack(MFRThings.meatNuggetRawItem, rand.nextInt(2));
 				else
-					if (rand.nextInt(5) != 0)
-						drop = new ItemStack(MFRThings.meatNuggetRawItem, rand.nextInt(2));
-					else
-						drop = new ItemStack(MFRThings.meatNuggetCookedItem, rand.nextInt(2));
+					drop = new ItemStack(MFRThings.meatNuggetCookedItem, rand.nextInt(2));
 			}
-			else if (this == MFRThings.chocolateMilkLiquid)
-			{
+			else if (this == MFRThings.chocolateMilkLiquid) {
 				if (rand.nextBoolean())
 					drop = new ItemStack(Items.dye, rand.nextInt(2), 3);
 			}
-			else if (this == MFRThings.mushroomSoupLiquid)
-			{
+			else if (this == MFRThings.mushroomSoupLiquid) {
 				if (rand.nextInt(5) == 0)
 					block = (rand.nextBoolean() ? Blocks.brown_mushroom : Blocks.red_mushroom);
+				else if (rand.nextBoolean())
+					drop = new ItemStack(Blocks.brown_mushroom, rand.nextInt(2));
 				else
-					if (rand.nextBoolean())
-						drop = new ItemStack(Blocks.brown_mushroom, rand.nextInt(2));
-					else
-						drop = new ItemStack(Blocks.red_mushroom, rand.nextInt(2));
+					drop = new ItemStack(Blocks.red_mushroom, rand.nextInt(2));
 			}
-			if (world.setBlock(x, y, z, block, 0, 3))
-			{
+			if (world.setBlock(x, y, z, block, 0, 3)) {
 				if (drop != null && drop.stackSize > 0)
 					this.dropBlockAsItem(world, x, y, z, drop);
 
@@ -231,44 +209,47 @@ public class BlockFactoryFluid extends BlockFluidCoFHBase implements IRedNetDeco
 		}
 	}
 
-	protected void fizz(World world, int x, int y, int z, Random rand)
-	{
+	protected void fizz(World world, int x, int y, int z, Random rand) {
+
 		world.playSoundEffect(x + 0.5D, y + 0.5D, z + 0.5D,
-				"random.fizz", 0.5F, 2.6F + (rand.nextFloat() - rand.nextFloat()) * 0.8F);
-		for (int l = 0; l < 8; ++l)
-		{
+			"random.fizz", 0.5F, 2.6F + (rand.nextFloat() - rand.nextFloat()) * 0.8F);
+		for (int l = 0; l < 8; ++l) {
 			world.spawnParticle("largesmoke",
-					x + rand.nextDouble(), y + rand.nextDouble(), z + rand.nextDouble(),
-					0.0D, 0.0D, 0.0D);
+				x + rand.nextDouble(), y + rand.nextDouble(), z + rand.nextDouble(),
+				0.0D, 0.0D, 0.0D);
 		}
 	}
 
 	@Override
-	public String getUnlocalizedName()
-	{
+	public String getUnlocalizedName() {
+
 		return "fluid." + this.unlocalizedName;
 	}
 
-	@SideOnly(Side.CLIENT)
 	@Override
-	public void registerBlockIcons(IIconRegister ir)
-	{
-		setIcons(ir.registerIcon("minefactoryreloaded:" + getUnlocalizedName()),
-				ir.registerIcon("minefactoryreloaded:" + getUnlocalizedName().replace(".still", ".flowing")));
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister ir) {
+
+		String pre = "minefactoryreloaded:", name = getUnlocalizedName();
+		if (!RegistryUtils.blockTextureExists(pre + name)) {
+			pre += "fluid/";
+		}
+		setIcons(ir.registerIcon(name = pre + name), ir.registerIcon(name.replace(".still", ".flowing")));
 	}
 
 	@SideOnly(Side.CLIENT)
-	public void setIcons(IIcon still, IIcon flowing)
-	{
+	public void setIcons(IIcon still, IIcon flowing) {
+
 		_iconStill = still;
 		_iconFlowing = flowing;
 		setParticleColor(color = RegistryUtils.getBlockTextureColor(still.getIconName()));
 	}
 
-	@SideOnly(Side.CLIENT)
 	@Override
-	public IIcon getIcon(int side, int meta)
-	{
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta) {
+
 		return side <= 1 ? _iconStill : _iconFlowing;
 	}
+
 }
