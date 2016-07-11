c2b220e3c85e2334dd5fe5655368bf08dd8abad3
skyboy <mr.wraith.06@gmail.com>
2016-07-11 16:08:39 -0400

    Fix #570

--- src/powercrystals/minefactoryreloaded/modhelpers/buildcraft/Buildcraft.java
+++ src/powercrystals/minefactoryreloaded/modhelpers/buildcraft/Buildcraft.java
@@ -8,44 +8,21 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.CustomProperty;
 import cpw.mods.fml.common.ModContainer;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
-import net.minecraft.block.Block;
 import net.minecraftforge.fluids.FluidRegistry;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
-import powercrystals.minefactoryreloaded.setup.MFRThings;
 
 @ChildMod(parent = MineFactoryReloadedCore.modId, mod = @Mod(modid = "MineFactoryReloaded|CompatBuildCraft",
 		name = "MFR Compat: BuildCraft",
 		version = MineFactoryReloadedCore.version,
-		dependencies = "after:MineFactoryReloaded;after:BuildCraft|Core",
+		dependencies = "after:MineFactoryReloaded;BuildCraftAPI|fuels",
 		customProperties = @CustomProperty(k = "cofhversion", v = "true")))
 public class Buildcraft {
-
-	private static String name(Block obj) {
-
-		return Block.blockRegistry.getNameForObject(obj);
-	}
-
-	@Mod.EventHandler
-	@Strippable("mod:BuildCraft|Core")
-	public void init(FMLInitializationEvent evt) {
-
-		for (Object[] q : new Object[][] { { 16, name(MFRThings.factoryDecorativeBrickBlock) },
-				{ 12, name(MFRThings.factoryDecorativeStoneBlock) } })
-			for (int i = (Integer) q[0]; i-- > 0;)
-				FMLInterModComms.sendMessage("BuildCraft|Core", "add-facade", q[1] + "@" + i);
-		String block = Block.blockRegistry.getNameForObject(MFRThings.factoryRoadBlock);
-		FMLInterModComms.sendMessage("BuildCraft|Core", "add-facade", block + "@0");
-		FMLInterModComms.sendMessage("BuildCraft|Core", "add-facade", block + "@1");
-		FMLInterModComms.sendMessage("BuildCraft|Core", "add-facade", block + "@4");
-	}
 
 	@Mod.EventHandler
 	@Strippable("api:BuildCraftAPI|fuels")
