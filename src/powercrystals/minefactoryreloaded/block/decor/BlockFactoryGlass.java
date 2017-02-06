bd4a36c65f71a11a1b3b3d091669b8d7d496f5a9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-02-06 17:43:00 +0100

    minor refactoring and fix so that stained glass doesn't display any overlay texture inside for different colors

--- src/powercrystals/minefactoryreloaded/block/decor/BlockFactoryGlass.java
+++ src/powercrystals/minefactoryreloaded/block/decor/BlockFactoryGlass.java
@@ -14,6 +14,7 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.property.IExtendedBlockState;
@@ -24,6 +25,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import powercrystals.minefactoryreloaded.MineFactoryReloadedCore;
 import powercrystals.minefactoryreloaded.api.rednet.connectivity.IRedNetDecorative;
 import powercrystals.minefactoryreloaded.gui.MFRCreativeTab;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlockFactoryGlass extends BlockGlass implements IRedNetDecorative
 {
@@ -56,15 +61,51 @@ public class BlockFactoryGlass extends BlockGlass implements IRedNetDecorative
 	}
 
 	@Override
-	public IBlockState getExtendedState(IBlockState state, IBlockAccess world, BlockPos pos) {
-
+	public IBlockState getExtendedState(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
 		IExtendedBlockState extState = (IExtendedBlockState) state;
 
+		Map<Integer, Boolean> connections = getConnections(world, pos);
+
 		for(EnumFacing facing : EnumFacing.VALUES) {
-			extState = extState.withProperty(CTM_VALUE[facing.ordinal()], getCTMValue(facing, world, pos));
+			extState = extState.withProperty(CTM_VALUE[facing.ordinal()], getCTMValue(connections, facing));
 		}
 
 		return extState;
+	}
+
+	private Map<Integer, Boolean> getConnections(IBlockAccess world, BlockPos pos)
+	{
+		Map<Integer, Boolean> connections = new HashMap<>();
+
+		for(EnumFacing facing : EnumFacing.VALUES) {
+			updateSideConnection(world, pos, connections, facing);
+		}
+
+		for(EnumFacing facing : EnumFacing.HORIZONTALS) {
+			updateDiagonalConnection(world, pos, connections, facing, EnumFacing.UP, 6);
+			updateDiagonalConnection(world, pos, connections, facing, EnumFacing.HORIZONTALS[(facing.getHorizontalIndex() + 1) % 4], 10);
+			updateDiagonalConnection(world, pos, connections, facing, EnumFacing.DOWN, 14);
+		}
+
+		return connections;
+	}
+
+	private void updateDiagonalConnection(IBlockAccess world, BlockPos pos, Map<Integer, Boolean> connections, EnumFacing facing,
+			EnumFacing secondFacing, int initialIndex)
+	{
+		if(connections.get(secondFacing.ordinal()) || connections.get(facing.ordinal())) {
+			IBlockState neighborState = world.getBlockState(pos.offset(secondFacing).offset(facing));
+			connections.put(initialIndex + facing.getHorizontalIndex(),	neighborState.getBlock() == this);
+		} else {
+			connections.put(initialIndex + facing.getHorizontalIndex(), false);
+		}
+	}
+
+	private void updateSideConnection(IBlockAccess world, BlockPos pos, Map<Integer, Boolean> connections, EnumFacing facing)
+	{
+		IBlockState stateNeigbor = world.getBlockState(pos.offset(facing));
+		connections.put(facing.ordinal(), stateNeigbor.getBlock() == this);
 	}
 
 	@Override
@@ -122,51 +163,39 @@ public class BlockFactoryGlass extends BlockGlass implements IRedNetDecorative
 		return BlockRenderLayer.TRANSLUCENT;
 	}
 
-	private int getCTMValue(EnumFacing side, IBlockAccess world, BlockPos pos) {
-
-		BlockPos posToCheck;
+	private int getCTMValue(Map<Integer, Boolean> connections, EnumFacing side)
+	{
 		boolean[] sides = new boolean[8];
-		if (side.getAxis() == EnumFacing.Axis.Y)
+		if (connections.get(side.ordinal())) {
+			//connected to stained glass on this side - make it a no overlay texture (only displayed if connected to different glass color)
+			Arrays.fill(sides, true);
+		}
+		else if (side.getAxis() == EnumFacing.Axis.Y)
 		{
-			posToCheck = pos.offset(EnumFacing.EAST);
-			sides[0] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.SOUTH);
-			sides[4] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.WEST);
-			sides[1] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.WEST);
-			sides[5] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.NORTH);
-			sides[3] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.NORTH);
-			sides[6] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.EAST);
-			sides[2] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.EAST);
-			sides[7] = world.getBlockState(posToCheck).getBlock().equals(this);
+			sides[0] = connections.get(EnumFacing.EAST.ordinal());
+			sides[4] = connections.get(10 + EnumFacing.EAST.getHorizontalIndex()); //SE
+			sides[1] = connections.get(EnumFacing.SOUTH.ordinal());
+			sides[5] = connections.get(10 + EnumFacing.SOUTH.getHorizontalIndex()); //SW
+			sides[3] = connections.get(EnumFacing.WEST.ordinal());
+			sides[6] = connections.get(10 + EnumFacing.WEST.getHorizontalIndex()); //NW
+			sides[2] = connections.get(EnumFacing.NORTH.ordinal());
+			sides[7] = connections.get(10 + EnumFacing.NORTH.getHorizontalIndex()); //NE
 		}
 		else
 		{
-			EnumFacing right = side.getAxis() == EnumFacing.Axis.Z ? EnumFacing.VALUES[(side.ordinal() + 2)] : EnumFacing.VALUES[(side.ordinal() - 2) ^ 1];
-			EnumFacing left = side.getAxis() == EnumFacing.Axis.Z ? EnumFacing.VALUES[(side.ordinal() + 2) ^ 1] : EnumFacing.VALUES[(side.ordinal() - 2)];
+			EnumFacing left = EnumFacing.HORIZONTALS[(side.getHorizontalIndex() + 1) % 4];
+			EnumFacing right = left.getOpposite();
 
-			posToCheck = pos.offset(right);
-			sides[0] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.DOWN);
-			sides[4] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(left);
-			sides[1] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(left);
-			sides[5] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.UP);
-			sides[3] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(EnumFacing.UP);
-			sides[6] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(right);
-			sides[2] = world.getBlockState(posToCheck).getBlock().equals(this);
-			posToCheck = posToCheck.offset(right);
-			sides[7] = world.getBlockState(posToCheck).getBlock().equals(this);
+			sides[0] = connections.get(right.ordinal()); //right
+			sides[4] = connections.get(14 + right.getHorizontalIndex()); //right down
+			sides[1] = connections.get(EnumFacing.DOWN.ordinal()); //down
+			sides[5] = connections.get(14 + left.getHorizontalIndex()); //left down
+			sides[3] = connections.get(left.ordinal()); //left
+			sides[6] = connections.get(6 + left.getHorizontalIndex()); //left up
+			sides[2] = connections.get(EnumFacing.UP.ordinal()); //up
+			sides[7] = connections.get(6 + right.getHorizontalIndex()); //right up
 		}
+		//TODO simplify this so that it directly returns index on sprite sheet
 		return toInt(sides) & 255;
 	}
 
