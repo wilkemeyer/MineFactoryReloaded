b6461daf4ff5f5d7f91d0ba0d5b8d3ab3bdf51d4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-02-19 02:17:53 +0100

    a bit of cleanup in core

--- src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
+++ src/powercrystals/minefactoryreloaded/MineFactoryReloadedCore.java
@@ -1,50 +1,10 @@
 package powercrystals.minefactoryreloaded;
 
 //this import brought to you by the department of redundancies department, the department that brought you this import
-import static powercrystals.minefactoryreloaded.MineFactoryReloadedCore.*;
-import static powercrystals.minefactoryreloaded.setup.MFRThings.*;
-
 import cofh.CoFHCore;
-import cofh.api.core.IInitializer;
 import cofh.core.world.WorldHandler;
-import com.google.common.collect.ImmutableList;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootEntry;
-import net.minecraft.world.storage.loot.LootEntryTable;
-import net.minecraft.world.storage.loot.LootPool;
-import net.minecraft.world.storage.loot.LootTableList;
-import net.minecraft.world.storage.loot.RandomValueRange;
-import net.minecraft.world.storage.loot.conditions.LootCondition;
-import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.CustomProperty;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLInterModComms;
-import net.minecraftforge.fml.common.event.FMLInterModComms.IMCEvent;
-import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
-import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent;
-import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent.MissingMapping;
-import net.minecraftforge.fml.common.event.FMLModIdMappingEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDispenser;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.dispenser.IBehaviorDispenseItem;
 import net.minecraft.entity.EntityList;
 import net.minecraft.init.Blocks;
@@ -53,105 +13,30 @@ import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidContainerRegistry.FluidContainerData;
 import net.minecraftforge.fluids.FluidRegistry;
-
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.CustomProperty;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.*;
+import net.minecraftforge.fml.common.event.FMLInterModComms.IMCEvent;
+import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent.MissingMapping;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
 import org.apache.logging.log4j.Logger;
-
-import powercrystals.minefactoryreloaded.block.BlockDetCord;
-import powercrystals.minefactoryreloaded.block.BlockFactoryMachine;
-import powercrystals.minefactoryreloaded.block.BlockFakeLaser;
-import powercrystals.minefactoryreloaded.block.BlockFertileSoil;
-import powercrystals.minefactoryreloaded.block.BlockRedNetLogic;
-import powercrystals.minefactoryreloaded.block.BlockRedNetPanel;
-import powercrystals.minefactoryreloaded.block.BlockRubberLeaves;
-import powercrystals.minefactoryreloaded.block.BlockRubberSapling;
-import powercrystals.minefactoryreloaded.block.BlockRubberWood;
-import powercrystals.minefactoryreloaded.block.BlockVineScaffold;
-import powercrystals.minefactoryreloaded.block.ItemBlockConveyor;
-import powercrystals.minefactoryreloaded.block.ItemBlockDetCord;
-import powercrystals.minefactoryreloaded.block.ItemBlockFactory;
-import powercrystals.minefactoryreloaded.block.ItemBlockFactoryLeaves;
-import powercrystals.minefactoryreloaded.block.ItemBlockFactoryMachine;
-import powercrystals.minefactoryreloaded.block.ItemBlockFactoryRoad;
-import powercrystals.minefactoryreloaded.block.ItemBlockFactoryTree;
-import powercrystals.minefactoryreloaded.block.ItemBlockRedNetLogic;
-import powercrystals.minefactoryreloaded.block.ItemBlockRedNetPanel;
-import powercrystals.minefactoryreloaded.block.ItemBlockTank;
-import powercrystals.minefactoryreloaded.block.ItemBlockVineScaffold;
-import powercrystals.minefactoryreloaded.block.decor.BlockDecorativeBricks;
-import powercrystals.minefactoryreloaded.block.decor.BlockDecorativeStone;
-import powercrystals.minefactoryreloaded.block.decor.BlockFactoryDecoration;
-import powercrystals.minefactoryreloaded.block.decor.BlockFactoryGlass;
-import powercrystals.minefactoryreloaded.block.decor.BlockFactoryGlassPane;
-import powercrystals.minefactoryreloaded.block.decor.BlockFactoryPlastic;
-import powercrystals.minefactoryreloaded.block.decor.BlockPinkSlime;
 import powercrystals.minefactoryreloaded.block.fluid.BlockExplodingFluid;
 import powercrystals.minefactoryreloaded.block.fluid.BlockFactoryFluid;
 import powercrystals.minefactoryreloaded.block.fluid.BlockPinkSlimeFluid;
-import powercrystals.minefactoryreloaded.block.fluid.BlockTank;
-import powercrystals.minefactoryreloaded.block.transport.BlockConveyor;
-import powercrystals.minefactoryreloaded.block.transport.BlockFactoryRoad;
-import powercrystals.minefactoryreloaded.block.transport.BlockPlasticPipe;
-import powercrystals.minefactoryreloaded.block.transport.BlockRailCargoDropoff;
-import powercrystals.minefactoryreloaded.block.transport.BlockRailCargoPickup;
-import powercrystals.minefactoryreloaded.block.transport.BlockRailPassengerDropoff;
-import powercrystals.minefactoryreloaded.block.transport.BlockRailPassengerPickup;
-import powercrystals.minefactoryreloaded.block.transport.BlockRedNetCable;
-import powercrystals.minefactoryreloaded.core.MFRDyeColor;
-import powercrystals.minefactoryreloaded.core.ReflectionHelper;
-import powercrystals.minefactoryreloaded.entity.DebugTracker;
-import powercrystals.minefactoryreloaded.entity.EntityFishingRod;
-import powercrystals.minefactoryreloaded.entity.EntityFlyingItem;
-import powercrystals.minefactoryreloaded.entity.EntityNeedle;
-import powercrystals.minefactoryreloaded.entity.EntityPinkSlime;
-import powercrystals.minefactoryreloaded.entity.EntityRocket;
-import powercrystals.minefactoryreloaded.entity.EntitySafariNet;
 import powercrystals.minefactoryreloaded.farmables.MFRFarmables;
 import powercrystals.minefactoryreloaded.gui.MFRGUIHandler;
-import powercrystals.minefactoryreloaded.item.ItemCeramicDye;
-import powercrystals.minefactoryreloaded.item.ItemFactoryBag;
-import powercrystals.minefactoryreloaded.item.ItemFactoryCup;
-import powercrystals.minefactoryreloaded.item.ItemLogicUpgradeCard;
-import powercrystals.minefactoryreloaded.item.ItemMilkBottle;
-import powercrystals.minefactoryreloaded.item.ItemPinkSlime;
-import powercrystals.minefactoryreloaded.item.ItemPlasticBoots;
-import powercrystals.minefactoryreloaded.item.ItemPortaSpawner;
-import powercrystals.minefactoryreloaded.item.ItemSafariNet;
-import powercrystals.minefactoryreloaded.item.ItemUpgrade;
-import powercrystals.minefactoryreloaded.item.base.ItemFactory;
-import powercrystals.minefactoryreloaded.item.base.ItemFactoryArmor;
-import powercrystals.minefactoryreloaded.item.base.ItemFactoryBucket;
-import powercrystals.minefactoryreloaded.item.base.ItemFactoryColored;
-import powercrystals.minefactoryreloaded.item.base.ItemFactoryFood;
-import powercrystals.minefactoryreloaded.item.gun.ItemNeedleGun;
-import powercrystals.minefactoryreloaded.item.gun.ItemPotatoCannon;
-import powercrystals.minefactoryreloaded.item.gun.ItemRocketLauncher;
-import powercrystals.minefactoryreloaded.item.gun.ItemSafariNetLauncher;
-import powercrystals.minefactoryreloaded.item.gun.ammo.ItemNeedlegunAmmoAnvil;
-import powercrystals.minefactoryreloaded.item.gun.ammo.ItemNeedlegunAmmoBlock;
-import powercrystals.minefactoryreloaded.item.gun.ammo.ItemNeedlegunAmmoFire;
-import powercrystals.minefactoryreloaded.item.gun.ammo.ItemNeedlegunAmmoStandard;
-import powercrystals.minefactoryreloaded.item.gun.ammo.ItemRocket;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeCure;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeGrowth;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeHealth;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeLiquid;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeSlime;
-import powercrystals.minefactoryreloaded.item.syringe.ItemSyringeZombie;
-import powercrystals.minefactoryreloaded.item.tool.ItemFactoryHammer;
-import powercrystals.minefactoryreloaded.item.tool.ItemFishingRod;
-import powercrystals.minefactoryreloaded.item.tool.ItemRedNetMemoryCard;
-import powercrystals.minefactoryreloaded.item.tool.ItemRedNetMeter;
-import powercrystals.minefactoryreloaded.item.tool.ItemRuler;
-import powercrystals.minefactoryreloaded.item.tool.ItemSpyglass;
-import powercrystals.minefactoryreloaded.item.tool.ItemStraw;
-import powercrystals.minefactoryreloaded.item.tool.ItemXpExtractor;
 import powercrystals.minefactoryreloaded.net.CommonProxy;
 import powercrystals.minefactoryreloaded.net.EntityHandler;
 import powercrystals.minefactoryreloaded.net.MFRPacket;
@@ -161,14 +46,15 @@ import powercrystals.minefactoryreloaded.setup.recipe.Vanilla;
 import powercrystals.minefactoryreloaded.setup.village.VillageCreationHandler;
 import powercrystals.minefactoryreloaded.setup.village.Zoologist;
 import powercrystals.minefactoryreloaded.tile.machine.TileEntityUnifier;
-import powercrystals.minefactoryreloaded.tile.rednet.TileEntityRedNetCable;
-import powercrystals.minefactoryreloaded.tile.rednet.TileEntityRedNetEnergy;
-import powercrystals.minefactoryreloaded.tile.rednet.TileEntityRedNetHistorian;
-import powercrystals.minefactoryreloaded.tile.rednet.TileEntityRedNetLogic;
-import powercrystals.minefactoryreloaded.tile.tank.TileEntityTank;
-import powercrystals.minefactoryreloaded.tile.transport.TileEntityConveyor;
-import powercrystals.minefactoryreloaded.tile.transport.TileEntityPlasticPipe;
 import powercrystals.minefactoryreloaded.world.MineFactoryReloadedWorldGen;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+import static powercrystals.minefactoryreloaded.MineFactoryReloadedCore.*;
+import static powercrystals.minefactoryreloaded.setup.MFRThings.*;
 
 @Mod(modid = modId, name = modName, version = version, dependencies = dependencies,
 		customProperties = @CustomProperty(k = "cofhversion", v = "true"))
@@ -199,18 +85,6 @@ public class MineFactoryReloadedCore extends BaseMod {
 	public static final String modelTextureFolder = textureFolder + "itemmodels/";
 	public static final String armorTextureFolder = textureFolder + "armor/";
 	public static final String modelFolder = prefix + "models/";
-
-	public static int renderIdConveyor = 1000;
-	public static int renderIdFactoryGlassPane = 1001;
-	public static int renderIdFluidTank = 1002;
-	public static int renderIdFluidClassic = 1003;
-	public static int renderIdRedNetLogic = 1004;
-	public static int renderIdVineScaffold = 1005;
-	public static int renderIdRedNetPanel = 1006;
-	public static int renderIdFactoryGlass = 1007;
-	public static int renderIdDetCord = 1008;
-	public static int renderIdRedNet = 1009;
-	public static int renderIdPPipe = 1009;
 
 	public static final ResourceLocation CHEST_GEN = new ResourceLocation("mfr:villageZoolologist");
 
@@ -432,52 +306,7 @@ public class MineFactoryReloadedCore extends BaseMod {
 
 	private void addChestGenItems() {
 
-/*  TODO add loot tables
-		//}
-
-		final WeightedRandomChestContent saplings = new WeightedRandomChestContent(new ItemStack(Blocks.SAPLING, 2), 1, 16, 7) {
-
-			@Override
-			public ItemStack[] generateChestContent(Random random, IInventory newInventory) {
-
-				ItemStack item = theItemId.copy();
-				item.setItemDamage(random.nextInt(6));
-				return ChestGenHooks.generateStacks(random, item, theMinimumChanceToGenerateItem, theMaximumChanceToGenerateItem);
-			}
-		};
-
-		//{ Fishing
-		TODO add implementation for plastic bag with loot defined by loot table - probably just use the loot nbt tag on first open to gen it 
-		FishingHooks.addTreasure(new WeightedRandomFishable(new ItemStack(plasticBagItem, 1, 0), 1) {
-
-			WeightedRandomChestContent[] loot = {
-					new WeightedRandomChestContent(new ItemStack(safariNetSingleItem, 1), 1, 1, 35),
-					new WeightedRandomChestContent(new ItemStack(Blocks.SAND, 4), 1, 16, 20),
-					new WeightedRandomChestContent(new ItemStack(plasticSheetItem, 16), 1, 16, 16),
-					new WeightedRandomChestContent(new ItemStack(plasticSheetItem, 23), 1, 23, 16),
-					new WeightedRandomChestContent(new ItemStack(plasticSheetItem, 6), 1, 6, 16),
-					new WeightedRandomChestContent(new ItemStack(Items.PAPER), 1, 16, 14),
-					new WeightedRandomChestContent(new ItemStack(spyglassItem, 1), 1, 1, 7),
-					saplings,
-					new WeightedRandomChestContent(new ItemStack(strawItem), 1, 1, 5),
-					new WeightedRandomChestContent(new ItemStack(Items.REEDS, 3), 1, 3, 2),
-					new WeightedRandomChestContent(new ItemStack(Items.PUMPKIN_SEEDS, 1), 1, 1, 2),
-					new WeightedRandomChestContent(new ItemStack(Items.MELON_SEEDS, 1), 1, 1, 2),
-					new WeightedRandomChestContent(new ItemStack(Items.DYE, 1, 4), 1, 1, 2),
-					new WeightedRandomChestContent(new ItemStack(Items.NETHERBRICK, 1), 1, 1, 1),
-			};
-
-			@Override
-			public ItemStack func_150708_a(Random r) {
-
-				ItemStack a = field_150711_b.copy();
-				a.setTagInfo("loot", new NBTTagByte((byte) 1));
-				InventoryContainerItemWrapper w = new InventoryContainerItemWrapper(a);
-				WeightedRandomChestContent.generateChestContents(r, loot, w, 1);
-				return w.getContainerStack();
-			}
-		});
-		//}
+/*  TODO add dim doors loot tables if needed
 
 		//{ DimensionalDoors chestgen compat
 		// reference weights[iron: 160; coal: 120; gold: 80; golden apple: 10]
