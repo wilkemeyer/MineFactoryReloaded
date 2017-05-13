f87db76a0aea3d53539a73ffc00402b7eae551cf
skyboy <mr.wraith.06@gmail.com>
2017-05-13 10:21:09 -0400

    Fix biome check on exploding fluids

--- src/powercrystals/minefactoryreloaded/block/fluid/BlockExplodingFluid.java
+++ src/powercrystals/minefactoryreloaded/block/fluid/BlockExplodingFluid.java
@@ -2,6 +2,7 @@ package powercrystals.minefactoryreloaded.block.fluid;
 
 import java.util.Random;
 
+import cofh.lib.world.biome.BiomeDictionaryArbiter;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Biomes;
@@ -9,19 +10,19 @@ import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import net.minecraftforge.common.BiomeDictionary;
 import powercrystals.minefactoryreloaded.setup.MFRConfig;
 
-public class BlockExplodingFluid extends BlockFactoryFluid
-{
-	public BlockExplodingFluid(String liquidName)
-	{
+public class BlockExplodingFluid extends BlockFactoryFluid {
+
+	public BlockExplodingFluid(String liquidName) {
+
 		super(liquidName);
 	}
 	
-	protected void explode(World world, BlockPos pos, Random rand, boolean noReplace)
-	{
-		if (noReplace || world.setBlockToAir(pos))
-		{
+	protected void explode(World world, BlockPos pos, Random rand, boolean noReplace) {
+
+		if (noReplace || world.setBlockToAir(pos)) {
 			if (isSourceBlock(world, pos) && MFRConfig.enableFuelExploding.getBoolean(true))
 				world.createExplosion(null, pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, 8, true);
 			fizz(world, pos, rand);
@@ -30,20 +31,19 @@ public class BlockExplodingFluid extends BlockFactoryFluid
 	}
 	
 	@Override
-	protected void checkCanStay(World world, BlockPos pos, Random rand)
-	{
-		if (world.getBiome(pos) == Biomes.HELL)
-		{
+	protected void checkCanStay(World world, BlockPos pos, Random rand) {
+
+		if (BiomeDictionary.isBiomeOfType(world.getBiome(pos), BiomeDictionary.Type.NETHER))  {
 			explode(world, pos, rand, false);
 		}
 	}
 
 	@Override
-	public void breakBlock(World world, BlockPos pos, IBlockState state)
-	{
-		if (world.getBlockState(pos).getBlock() == Blocks.FIRE)
-		{
+	public void breakBlock(World world, BlockPos pos, IBlockState state) {
+
+		if (world.getBlockState(pos).getBlock() == Blocks.FIRE) {
 			explode(world, pos, world.rand, true);
 		}
 	}
+
 }
