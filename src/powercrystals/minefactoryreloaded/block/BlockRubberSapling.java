f389dcd0d9c98d8cd27097b8d31e030a2cf063e6
skyboy <mr.wraith.06@gmail.com>
2017-05-13 08:24:36 -0400

    Use BlockSapling's STAGE property

--- src/powercrystals/minefactoryreloaded/block/BlockRubberSapling.java
+++ src/powercrystals/minefactoryreloaded/block/BlockRubberSapling.java
@@ -193,7 +193,7 @@ public class BlockRubberSapling extends BlockBush implements IRedNetNoConnection
 	}
 	
 	////// Vanilla BlockSapling fuctionality (because it has call in constructor because of which it can't be inherited) //////
-	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+	public static final PropertyInteger STAGE = BlockSapling.STAGE;
 	protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
 
 	@Override
