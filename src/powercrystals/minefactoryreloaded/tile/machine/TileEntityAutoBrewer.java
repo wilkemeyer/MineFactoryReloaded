3b8f6ff0d821a0600d7fdc15188e85082159ff9a
skyboy <mr.wraith.06@gmail.com>
2017-06-02 05:08:11 -0400

    Fix ingredient consumption logic in Auto-Brewer
    Also remove checks for ItemPotion, use isValidInput instead

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoBrewer.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityAutoBrewer.java
@@ -132,7 +132,7 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 
 			ItemStack ingredient = _inventory[getTemplateSlot(row)];
 
-			if (current != null && current.getItem() instanceof ItemPotion) {
+			if (current != null) {
 				if (ingredient == null) {
 					_inventory[getProcessSlot(row + 1)] = current;
 					_inventory[getProcessSlot(row)] = null;
@@ -140,11 +140,12 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 				}
 				for (int i = 0; i < 3; i++) {
 					int slot = getResourceSlot(row, i);
-					if (ingredient.stackSize <= 1 && !UtilInventory.stacksEqual(_inventory[slot], ingredient)) {
+					if (ingredient.stackSize <= 0 && !UtilInventory.stacksEqual(_inventory[slot], ingredient)) {
 						continue;
 					}
 
-					ItemStack newPotion = this.getPotionResult(current, ingredient);
+					ItemStack newPotion = this.getPotionResult(current, ingredient); // TODO: are there mods we need to be concerned about?
+					// if they modify the ingredient stack, we need either defensive copies, or to pass the storage slot stack
 
 					if (newPotion != null && current != newPotion) {
 						_inventory[getProcessSlot(row + 1)] = newPotion;
@@ -157,7 +158,7 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 					if (current == newPotion)
 						break;
 
-					if (ingredient.stackSize > 1) {
+					if (ingredient.stackSize > 0) {
 						--ingredient.stackSize;
 						break;
 					}
@@ -184,9 +185,15 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 			return false;
 		}
 
-		boolean hasIngredients = false;
-		for (int i = 0; i < 3; i++) {
-			if (UtilInventory.stacksEqual(_inventory[getTemplateSlot(row)], _inventory[getResourceSlot(row, i)])) {
+		ItemStack ingredient = _inventory[getTemplateSlot(row)];
+
+		if (!BrewingRecipeRegistry.isValidIngredient(ingredient)) {
+			return false;
+		}
+
+		boolean hasIngredients = ingredient.stackSize > 0;
+		if (!hasIngredients) for (int i = 0; i < 3; i++) {
+			if (UtilInventory.stacksEqual(ingredient, _inventory[getResourceSlot(row, i)])) {
 				hasIngredients = true;
 				break;
 			}
@@ -195,14 +202,7 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 			return false;
 		}
 
-		ItemStack ingredient = _inventory[getTemplateSlot(row)];
-
-		if (!BrewingRecipeRegistry.isValidIngredient(ingredient)) {
-			return false;
-		}
-
-		if (_inventory[getProcessSlot(row)] != null &&
-				_inventory[getProcessSlot(row)].getItem() instanceof ItemPotion) {
+		if (_inventory[getProcessSlot(row)] != null) {
 			ItemStack newPotion = this.getPotionResult(_inventory[getProcessSlot(row)], ingredient);
 
 			if (newPotion != null && _inventory[getProcessSlot(row)] != newPotion) {
@@ -245,7 +245,7 @@ public class TileEntityAutoBrewer extends TileEntityFactoryPowered {
 		if (row == 6) return false;
 		if (column == 1) return false;
 		if (column == 0) return _inventory[getTemplateSlot(row)] != null &&
-				itemstack.getItem() instanceof ItemPotion &&
+				BrewingRecipeRegistry.isValidInput(itemstack) &&
 				(row == 0 || _inventory[getTemplateSlot(row - 1)] == null);
 		return ingredientsEqual(_inventory[getTemplateSlot(row)], itemstack);
 	}
