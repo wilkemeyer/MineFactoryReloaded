481ea89ae0ee7ecbd9645ad4770ee97506fa027b
skyboy <mr.wraith.06@gmail.com>
2017-06-11 14:44:05 -0400

    Fix grinders setting a world to easy when peaceful

--- src/powercrystals/minefactoryreloaded/world/GrindingWorldServer.java
+++ src/powercrystals/minefactoryreloaded/world/GrindingWorldServer.java
@@ -13,63 +13,57 @@ import net.minecraft.world.WorldServer;
 import powercrystals.minefactoryreloaded.tile.base.TileEntityFactoryPowered;
 import powercrystals.minefactoryreloaded.tile.machine.TileEntityGrinder;
 
-public class GrindingWorldServer extends WorldServerProxy
-{
+public class GrindingWorldServer extends WorldServerProxy {
+
 	protected TileEntityFactoryPowered grinder;
 	protected boolean allowSpawns;
 	protected ArrayList<Entity> entitiesToGrind = new ArrayList<Entity>();
 
-	public GrindingWorldServer(WorldServer world, TileEntityFactoryPowered grinder)
-	{
+	public GrindingWorldServer(WorldServer world, TileEntityFactoryPowered grinder) {
+
 		this(world, grinder, false);
 	}
 
-	public GrindingWorldServer(WorldServer world, TileEntityFactoryPowered grinder, boolean allowSpawns)
-	{
+	public GrindingWorldServer(WorldServer world, TileEntityFactoryPowered grinder, boolean allowSpawns) {
+
 		super(world);
 		this.grinder = grinder;
 		this.allowSpawns = allowSpawns;
 	}
 
-	public void setAllowSpawns(boolean allow)
-	{
+	public void setAllowSpawns(boolean allow) {
+
 		this.allowSpawns = allow;
 	}
 
-	public void setMachine(TileEntityFactoryPowered machine)
-	{
+	public void setMachine(TileEntityFactoryPowered machine) {
+
 		this.grinder = machine;
 	}
 
 	@Override
-	public boolean spawnEntityInWorld(Entity entity)
-	{
-		if (grinder != null)
-		{
-			if (entity instanceof EntityItem)
-			{
-				if (grinder.manageSolids())
-				{
-					ItemStack drop = ((EntityItem)entity).getEntityItem();
-					if (drop != null) grinder.doDrop(drop);
+	public boolean spawnEntityInWorld(Entity entity) {
+
+		if (grinder != null) {
+			if (entity instanceof EntityItem) {
+				if (grinder.manageSolids()) {
+					ItemStack drop = ((EntityItem) entity).getEntityItem();
+					if (drop != null)
+						grinder.doDrop(drop);
 				}
 				entity.setDead();
 				return true;
-			}
-			else if (entity instanceof EntityXPOrb)
-			{
-				EntityXPOrb orb = (EntityXPOrb)entity;
-				if (grinder instanceof TileEntityGrinder)
-				{
-					((TileEntityGrinder)grinder).acceptXPOrb(orb);
+			} else if (entity instanceof EntityXPOrb) {
+				EntityXPOrb orb = (EntityXPOrb) entity;
+				if (grinder instanceof TileEntityGrinder) {
+					((TileEntityGrinder) grinder).acceptXPOrb(orb);
 				}
 				entity.setDead();
 				return true; // consume any orbs not made into essence
 			}
 		}
 
-		if (allowSpawns)
-		{
+		if (allowSpawns) {
 			entity.worldObj = this.proxiedWorld;
 			return super.spawnEntityInWorld(entity);
 		}
@@ -77,14 +71,17 @@ public class GrindingWorldServer extends WorldServerProxy
 		return true;
 	}
 
-	public boolean addEntityForGrinding(Entity entity)
-	{
+	public EnumDifficulty getDifficulty() {
+
+		return super.getDifficulty() == EnumDifficulty.PEACEFUL ? EnumDifficulty.EASY : super.getDifficulty();
+	}
+
+	public boolean addEntityForGrinding(Entity entity) {
+
 		cofh_updateProps();
-		if (getDifficulty() == EnumDifficulty.PEACEFUL)
-			getWorldInfo().setDifficulty(EnumDifficulty.EASY);
-		if(entity.worldObj == this) return true;
-		if(entity.worldObj == this.proxiedWorld)
-		{
+		if (entity.worldObj == this)
+			return true;
+		if (entity.worldObj == this.proxiedWorld) {
 			entity.worldObj = this;
 			entitiesToGrind.add(entity);
 			return true;
@@ -92,21 +89,21 @@ public class GrindingWorldServer extends WorldServerProxy
 		return false;
 	}
 
-	public void clearReferences()
-	{
-		for(Entity ent : entitiesToGrind)
-		{
-			if(ent.worldObj == this) ent.worldObj = this.proxiedWorld;
+	public void clearReferences() {
+
+		for (Entity ent : entitiesToGrind) {
+			if (ent.worldObj == this)
+				ent.worldObj = this.proxiedWorld;
 		}
 		entitiesToGrind.clear();
 	}
 
-	public void cleanReferences()
-	{
-		for(int i = entitiesToGrind.size(); i --> 0;)
-		{
+	public void cleanReferences() {
+
+		for (int i = entitiesToGrind.size(); i-- > 0; ) {
 			Entity ent = entitiesToGrind.get(i);
-			if(ent.isDead) entitiesToGrind.remove(ent);
+			if (ent.isDead)
+				entitiesToGrind.remove(ent);
 		}
 	}
 
