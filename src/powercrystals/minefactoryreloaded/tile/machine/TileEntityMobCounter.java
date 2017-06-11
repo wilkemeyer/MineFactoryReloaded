0d02b1b69d463f29bc1c594692bf48e435a39bab
skyboy <mr.wraith.06@gmail.com>
2017-06-11 12:26:18 -0400

    Simplify MobCounter

--- src/powercrystals/minefactoryreloaded/tile/machine/TileEntityMobCounter.java
+++ src/powercrystals/minefactoryreloaded/tile/machine/TileEntityMobCounter.java
@@ -7,41 +7,39 @@ import powercrystals.minefactoryreloaded.setup.MFRThings;
 import powercrystals.minefactoryreloaded.setup.Machine;
 import powercrystals.minefactoryreloaded.tile.base.TileEntityFactory;
 
-public class TileEntityMobCounter extends TileEntityFactory
-{
+public class TileEntityMobCounter extends TileEntityFactory {
+
 	private int _lastMobCount;
 
-	public TileEntityMobCounter()
-	{
+	public TileEntityMobCounter() {
+
 		super(Machine.MobCounter);
 		createEntityHAM(this);
 		setCanRotate(true);
 	}
 
 	@Override
-	public void update()
-	{
+	public void update() {
+
 		super.update();
 
-		if (worldObj == null)
-		{
+		if (worldObj == null) {
 			return;
 		}
 
 		int mobCount = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, _areaManager.getHarvestArea().toAxisAlignedBB()).size();
-		if (mobCount != _lastMobCount)
-		{
+		if (mobCount != _lastMobCount) {
 			_lastMobCount = mobCount;
-			if (!worldObj.isRemote)
-			{
-				worldObj.notifyNeighborsOfStateChange(pos, MFRThings.machineBlocks.get(Machine.MobCounter.getBlockIndex()));
+			if (!worldObj.isRemote) {
+				worldObj.notifyNeighborsOfStateChange(pos, this._machine.getBlock());
 			}
 		}
 	}
 
 	@Override
-	public int getRedNetOutput(EnumFacing side)
-	{
+	public int getRedNetOutput(EnumFacing side) {
+
 		return _lastMobCount;
 	}
+
 }
